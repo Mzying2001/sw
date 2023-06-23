1df7cfc6f110547d202b29172a39d0a42c1f5444
Mzying2001 <Mzying2001@qq.com>
2023-06-23 13:33:26 +0800

    添加注释

--- sw/inc/WndBase.h
+++ sw/inc/WndBase.h
@@ -17,6 +17,10 @@ namespace sw
 
     protected:
         static const PCWSTR WndBaseClassName;
+
+        /**
+         * @brief 该函数会调用DefWindowProcW
+         */
         static LRESULT DefaultWndProc(const ProcMsg &refMsg);
 
     private:
@@ -40,13 +44,47 @@ namespace sw
 
     protected:
         WndBase();
+
+        /**
+         * @brief 初始化WndBase，该函数会调用CreateWindowExW创建窗口
+         */
         void InitWndBase(DWORD dwExStyle, LPCWSTR lpWindowName, DWORD dwStyle, HWND hWndParent, HMENU hMenu);
+
+        /**
+         * @brief 对WndProc的封装
+         */
         virtual LRESULT WndProc(const ProcMsg &refMsg);
+
+        /**
+         * @brief  接收到WM_CREATE时调用该函数
+         * @return 若已处理该消息则返回true，否则返回false以调用DefWindowProcW
+         */
         virtual bool OnCreate();
+
+        /**
+         * @brief  接收到WM_DESTROY时调用该函数
+         * @return 若已处理该消息则返回true，否则返回false以调用DefWindowProcW
+         */
         virtual bool OnDestroy();
+
+        /**
+         * @brief  接收到WM_PAINT时调用该函数
+         * @return 若已处理该消息则返回true，否则返回false以调用DefWindowProcW
+         */
         virtual bool OnPaint();
+
+        /**
+         * @brief  接收到WM_MOVE时调用该函数
+         * @return 若已处理该消息则返回true，否则返回false以调用DefWindowProcW
+         */
         virtual bool OnMove(double newLeft, double newTop);
+
+        /**
+         * @brief  接收到WM_SIZE时调用该函数
+         * @return 若已处理该消息则返回true，否则返回false以调用DefWindowProcW
+         */
         virtual bool OnSize(double newClientWidth, double newClientHeight);
+
         /**
          * @brief         Text属性被赋值时调用此函数
          * @param newText Text的新值
