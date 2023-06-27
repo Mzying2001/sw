80b9f7b266d479c9b21d6caba25ffe9a83bf4ec0
Mzying2001 <Mzying2001@qq.com>
2023-06-28 00:32:32 +0800

    添加ILayout接口

---
+++ sw/inc/ILayout.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "Rect.h"
+#include "Size.h"
+
+namespace sw
+{
+    /**
+     * @brief 布局接口
+     */
+    class ILayout
+    {
+    public:
+        /**
+         * @brief 获取子控件的数量
+         */
+        virtual int GetChildLayoutCount() = 0;
+
+        /**
+         * @brief 获取对应索引处的子控件
+         */
+        virtual ILayout &GetChildLayoutAt() = 0;
+
+        /**
+         * @brief 获取控件所需尺寸
+         */
+        virtual Size GetDesireSize() = 0;
+
+        /**
+         * @brief               测量控件所需尺寸
+         * @param availableSize 可用的尺寸
+         */
+        virtual void Measure(const Size &availableSize) = 0;
+
+        /**
+         * @brief           安排控件位置
+         * @param finalSize 最终控件所安排的位置
+         */
+        virtual void Arrange(const Rect &finalSize) = 0;
+    };
+}
