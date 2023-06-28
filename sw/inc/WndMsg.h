cdf7978a63672ffa5ab0da46001a661784bcac01
enterausername11111 <u4i_mkb0nwvw5@dingtalk.com>
2023-06-28 11:49:27 +0800

    添加WM_LayoutUpdated

--- sw/inc/WndMsg.h
+++ sw/inc/WndMsg.h
@@ -15,6 +15,11 @@ namespace sw
         WM_SimpleWindowBegin = WM_USER + 0x3000,
 
         /**
+         * @brief 控件布局发生变化时控件所在顶级窗口将收到该消息，wParam和lParam均未使用
+         */
+        WM_LayoutUpdated,
+
+        /**
          * @brief 父窗口接收到WM_COMMAND时向控件发该消息，wParam和lParam按原样发回
          */
         WM_ParentReceivedCommand,
