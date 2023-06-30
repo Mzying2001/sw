33147a35a7fef6aaaa9c886dd98bb8c1c3832e07
enterausername11111 <u4i_mkb0nwvw5@dingtalk.com>
2023-06-30 11:42:53 +0800

    添加FillLayout类

---
+++ sw/inc/FillLayout.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "LayoutHost.h"
+
+namespace sw
+{
+    class FillLayout : public LayoutHost
+    {
+    public:
+        /**
+         * @brief 计算所需尺寸
+         */
+        virtual void MeasureOverride(Size &availableSize);
+
+        /**
+         * @brief 安排控件
+         */
+        virtual void ArrangeOverride(Size &finalSize);
+    };
+}
---
+++ sw/src/FillLayout.cpp
@@ -0,0 +1,20 @@
+#include "FillLayout.h"
+
+void sw::FillLayout::MeasureOverride(Size &availableSize)
+{
+    int count = this->GetChildLayoutCount();
+    for (int i = 0; i < count; ++i) {
+        ILayout &item = this->GetChildLayoutAt(i);
+        item.Measure(availableSize);
+    }
+    this->SetDesireSize(availableSize);
+}
+
+void sw::FillLayout::ArrangeOverride(Size &finalSize)
+{
+    int count = this->GetChildLayoutCount();
+    for (int i = 0; i < count; ++i) {
+        ILayout &item = this->GetChildLayoutAt(i);
+        item.Arrange(Rect(0, 0, finalSize.width, finalSize.width));
+    }
+}
