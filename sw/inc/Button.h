9f01b5935c0645f98a74fa2275f55b5eb06433e0
Mzying2001 <Mzying2001@qq.com>
2023-07-09 13:53:49 +0800

    按钮获取到焦点时改变样式

--- sw/inc/Button.h
+++ sw/inc/Button.h
@@ -8,5 +8,20 @@ namespace sw
     {
     public:
         Button();
+
+    protected:
+        /**
+         * @brief           接收到WM_SETFOCUS时调用该函数
+         * @param hPreFocus 丢失焦点的hwnd，可能为NULL
+         * @return          若已处理该消息则返回true，否则返回false以调用DefaultWndProc
+         */
+        virtual bool OnSetFocus(HWND hPreFocus);
+
+        /**
+         * @brief            接收到WM_KILLFOCUS时调用该函数
+         * @param hNextFocus 接收到焦点的hwnd，可能为NULL
+         * @return           若已处理该消息则返回true，否则返回false以调用DefaultWndProc
+         */
+        virtual bool OnKillFocus(HWND hNextFocus);
     };
 }
--- sw/src/Button.cpp
+++ sw/src/Button.cpp
@@ -1,7 +1,24 @@
 #include "Button.h"
 
+#define BUTTONSTYLE_DEFAULT (WS_CHILD | WS_VISIBLE | BS_NOTIFY | BS_PUSHBUTTON)
+#define BUTTONSTYLE_FOCUSED (WS_CHILD | WS_VISIBLE | BS_NOTIFY | BS_DEFPUSHBUTTON)
+
 sw::Button::Button()
 {
-    this->InitButtonBase(L"Button", WS_CHILD | WS_VISIBLE | BS_NOTIFY | BS_PUSHBUTTON);
+    this->InitButtonBase(L"Button", BUTTONSTYLE_DEFAULT);
     this->Rect = sw::Rect(0, 0, 70, 30);
+}
+
+bool sw::Button::OnSetFocus(HWND hPreFocus)
+{
+    this->SetStyle(BUTTONSTYLE_FOCUSED);
+    this->Redraw();
+    return true;
+}
+
+bool sw::Button::OnKillFocus(HWND hNextFocus)
+{
+    this->SetStyle(BUTTONSTYLE_DEFAULT);
+    this->Redraw();
+    return true;
 }
