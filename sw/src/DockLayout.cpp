8001522c8548de069b81761fde98fe4350438100
Mzying2001 <Mzying2001@qq.com>
2023-07-17 00:07:40 +0800

    Update DockLayout.cpp

--- sw/src/DockLayout.cpp
+++ sw/src/DockLayout.cpp
@@ -1,5 +1,11 @@
 #include "DockLayout.h"
 
+static sw::DockLayout::DockLayoutTag _GetDockLayoutTag(sw::ILayout &item)
+{
+    uint32_t tag = item.GetLayoutTag();
+    return tag > sw::DockLayout::Bottom ? sw::DockLayout::Left : (sw::DockLayout::DockLayoutTag)tag;
+}
+
 void sw::DockLayout::MeasureOverride(Size &availableSize)
 {
     // TODO
