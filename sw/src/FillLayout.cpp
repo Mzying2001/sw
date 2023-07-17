c4cb3d50d98e336ad026eba4452df827cc131066
Mzying2001 <Mzying2001@qq.com>
2023-07-18 00:29:09 +0800

    Update FillLayout.cpp

--- sw/src/FillLayout.cpp
+++ sw/src/FillLayout.cpp
@@ -2,12 +2,16 @@
 
 void sw::FillLayout::MeasureOverride(Size &availableSize)
 {
+    Size desireSize;
     int count = this->GetChildLayoutCount();
     for (int i = 0; i < count; ++i) {
         ILayout &item = this->GetChildLayoutAt(i);
         item.Measure(availableSize);
+        Size itemDesireSize = item.GetDesireSize();
+        desireSize.width    = max(desireSize.width, itemDesireSize.width);
+        desireSize.height   = max(desireSize.height, itemDesireSize.height);
     }
-    this->SetDesireSize(availableSize);
+    this->SetDesireSize(desireSize);
 }
 
 void sw::FillLayout::ArrangeOverride(Size &finalSize)
