c3ed37e98a103736592e980038cecc71c29cc42d
enterausername11111 <u4i_mkb0nwvw5@dingtalk.com>
2023-07-18 10:21:18 +0800

    添加LastChildFill属性

--- sw/inc/DockPanel.h
+++ sw/inc/DockPanel.h
@@ -11,6 +11,12 @@ namespace sw
         DockLayout _dockLayout = DockLayout();
 
     public:
+        /**
+         * @brief 最后一个子元素是否填充剩余空间
+         */
+        const Property<bool> LastChildFill;
+
+    public:
         DockPanel();
     };
 }
--- sw/src/DockPanel.cpp
+++ sw/src/DockPanel.cpp
@@ -1,6 +1,16 @@
 #include "DockPanel.h"
 
 sw::DockPanel::DockPanel()
+    : LastChildFill(
+          // get
+          [&]() -> const bool & {
+              return this->_dockLayout.lastChildFill;
+          },
+          // set
+          [&](const bool &value) {
+              this->_dockLayout.lastChildFill = value;
+              this->NotifyLayoutUpdated();
+          })
 {
     this->Layout = &this->_dockLayout;
 }
