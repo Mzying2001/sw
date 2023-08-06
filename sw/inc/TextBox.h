2b48e4ce051a67f697fbbd51f1d0c6926993cbc3
Mzying2001 <Mzying2001@qq.com>
2023-08-06 15:11:26 +0800

    正确获取Text

--- sw/inc/TextBox.h
+++ sw/inc/TextBox.h
@@ -6,7 +6,26 @@ namespace sw
 {
     class TextBox : public Control
     {
+    private:
+        /**
+         * @brief 在读取Text属性时用于判断是否需要更新储存的文本
+         */
+        bool _isTextChanged = false;
+
     public:
         TextBox();
+
+    protected:
+        /**
+         * @brief  获取窗口文本
+         * @return _text字段
+         */
+        virtual std::wstring &GetText() override;
+
+        /**
+         * @brief      当父窗口接收到控件的WM_COMMAND时调用该函数
+         * @param code 通知代码
+         */
+        virtual void OnCommand(int code) override;
     };
 }
--- sw/src/TextBox.cpp
+++ sw/src/TextBox.cpp
@@ -5,3 +5,40 @@ sw::TextBox::TextBox()
     this->InitControl(L"EDIT", L"", WS_CHILD | WS_VISIBLE | ES_LEFT | ES_AUTOHSCROLL | ES_AUTOVSCROLL, WS_EX_CLIENTEDGE);
     this->Rect = sw::Rect(0, 0, 100, 24);
 }
+
+std::wstring &sw::TextBox::GetText()
+{
+    std::wstring &refText = this->WndBase::GetText();
+
+    if (!this->_isTextChanged) {
+        return refText;
+    }
+
+    HWND hwnd = this->Handle;
+    int len   = GetWindowTextLengthW(hwnd);
+
+    if (len > 0) {
+        wchar_t *buf = new wchar_t[len + 1];
+        GetWindowTextW(hwnd, buf, len + 1);
+        refText = buf;
+        delete[] buf;
+    } else {
+        refText = L"";
+    }
+
+    this->_isTextChanged = false;
+    return refText;
+}
+
+void sw::TextBox::OnCommand(int code)
+{
+    switch (code) {
+        case EN_CHANGE: {
+            this->_isTextChanged = true;
+            break;
+        }
+
+        default:
+            break;
+    }
+}
