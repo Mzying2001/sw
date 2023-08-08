d686b28236e2f67e27bce1376f87b6b5082db0d3
Mzying2001 <Mzying2001@qq.com>
2023-08-08 21:22:22 +0800

    使用constexpr替换宏

--- sw/src/Button.cpp
+++ sw/src/Button.cpp
@@ -1,24 +1,24 @@
 #include "Button.h"
 
-#define BUTTONSTYLE_DEFAULT (WS_CHILD | WS_VISIBLE | BS_NOTIFY | BS_PUSHBUTTON)
-#define BUTTONSTYLE_FOCUSED (WS_CHILD | WS_VISIBLE | BS_NOTIFY | BS_DEFPUSHBUTTON)
+static constexpr DWORD _ButtonStyle_Default = WS_CHILD | WS_VISIBLE | BS_NOTIFY | BS_PUSHBUTTON;
+static constexpr DWORD _ButtonStyle_Focused = WS_CHILD | WS_VISIBLE | BS_NOTIFY | BS_DEFPUSHBUTTON;
 
 sw::Button::Button()
 {
-    this->InitButtonBase(L"Button", BUTTONSTYLE_DEFAULT, 0);
+    this->InitButtonBase(L"Button", _ButtonStyle_Default, 0);
     this->Rect = sw::Rect(0, 0, 70, 30);
 }
 
 bool sw::Button::OnSetFocus(HWND hPreFocus)
 {
-    this->SetStyle(BUTTONSTYLE_FOCUSED);
+    this->SetStyle(_ButtonStyle_Focused);
     this->Redraw();
     return this->ButtonBase::OnSetFocus(hPreFocus);
 }
 
 bool sw::Button::OnKillFocus(HWND hNextFocus)
 {
-    this->SetStyle(BUTTONSTYLE_DEFAULT);
+    this->SetStyle(_ButtonStyle_Default);
     this->Redraw();
     return this->ButtonBase::OnKillFocus(hNextFocus);
 }
