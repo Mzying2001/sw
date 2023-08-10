9744ebac338461d0da0a3c1348600fbe9f1bc6b8
Mzying2001 <Mzying2001@qq.com>
2023-08-10 21:54:18 +0800

    实现列表框

--- sw/src/ListBox.cpp
+++ sw/src/ListBox.cpp
@@ -2,62 +2,81 @@
 
 sw::ListBox::ListBox()
 {
+    this->InitControl(L"LISTBOX", L"", WS_CHILD | WS_VISIBLE | WS_BORDER | WS_VSCROLL | LBS_NOTIFY, 0);
+    this->Rect = sw::Rect(0, 0, 150, 200);
 }
 
 int sw::ListBox::GetItemsCount()
 {
-    // TODO
-    return 0;
+    return (int)this->SendMessageW(LB_GETCOUNT, 0, 0);
 }
 
 int sw::ListBox::GetSelectedIndex()
 {
-    // TODO
-    return 0;
+    return (int)this->SendMessageW(LB_GETCURSEL, 0, 0);
 }
 
 void sw::ListBox::SetSelectedIndex(int index)
 {
-    // TODO
+    this->SendMessageW(LB_SETCURSEL, index, 0);
 }
 
 std::wstring sw::ListBox::GetSelectedItem()
 {
-    // TODO
-    return std::wstring();
+    return this->GetItemAt(this->GetSelectedIndex());
 }
 
 void sw::ListBox::Clear()
 {
-    // TODO
+    this->SendMessageW(LB_RESETCONTENT, 0, 0);
 }
 
 std::wstring sw::ListBox::GetItemAt(int index)
 {
-    // TODO
-    return std::wstring();
+    int len = (int)this->SendMessageW(LB_GETTEXTLEN, index, 0);
+
+    if (len <= 0) {
+        return L"";
+    }
+
+    wchar_t *buf = new wchar_t[len + 1];
+    this->SendMessageW(LB_GETTEXT, index, reinterpret_cast<LPARAM>(buf));
+
+    std::wstring result = buf;
+
+    delete[] buf;
+    return result;
 }
 
 bool sw::ListBox::AddItem(const std::wstring &item)
 {
-    // TODO
-    return false;
+    int count = this->GetItemsCount();
+    this->SendMessageW(LB_ADDSTRING, 0, reinterpret_cast<LPARAM>(item.c_str()));
+    return this->GetItemsCount() == count + 1;
 }
 
 bool sw::ListBox::InsertItem(int index, const std::wstring &item)
 {
-    // TODO
-    return false;
+    int count = this->GetItemsCount();
+    this->SendMessageW(LB_INSERTSTRING, index, reinterpret_cast<LPARAM>(item.c_str()));
+    return this->GetItemsCount() == count + 1;
 }
 
 bool sw::ListBox::UpdateItem(int index, const std::wstring &newValue)
 {
-    // TODO
-    return false;
+    bool selected = this->GetSelectedIndex() == index;
+    bool updated  = this->RemoveItemAt(index) && this->InsertItem(index, newValue.c_str());
+
+    if (updated && selected) {
+        this->SetSelectedIndex(index);
+    }
+
+    return updated;
 }
 
 bool sw::ListBox::RemoveItemAt(int index)
 {
-    // TODO
-    return false;
+    int count = this->GetItemsCount();
+    this->SendMessageW(LB_DELETESTRING, index, 0);
+    return this->GetItemsCount() == count - 1;
 }
