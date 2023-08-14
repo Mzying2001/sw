f57a3adf194014865a27f0b9916df1e1e392b744
Mzying2001 <Mzying2001@qq.com>
2023-08-14 18:41:54 +0800

    实现ProgressBar

--- sw/inc/ProgressBar.h
+++ sw/inc/ProgressBar.h
@@ -6,10 +6,40 @@
 namespace sw
 {
     /**
+     * @brief 进度条状态
+     */
+    enum class ProgressBarState {
+        Normal = PBST_NORMAL, // 正常
+        Error  = PBST_ERROR,  // 错误
+        Paused = PBST_PAUSED, // 暂停
+    };
+
+    /**
      * @brief 进度条控件
      */
     class ProgressBar : public Control
     {
+    public:
+        /**
+         * @brief 进度范围的下限
+         */
+        const Property<uint16_t> Minimum;
+
+        /**
+         * @brief 进度范围的上限
+         */
+        const Property<uint16_t> Maximum;
+
+        /**
+         * @brief 当前进度值
+         */
+        const Property<uint16_t> Value;
+
+        /**
+         * @brief 进度条状态
+         */
+        const Property<ProgressBarState> State;
+
     public:
         /**
          * @brief 初始化进度条
--- sw/src/ProgressBar.cpp
+++ sw/src/ProgressBar.cpp
@@ -1,7 +1,56 @@
 #include "ProgressBar.h"
 
 sw::ProgressBar::ProgressBar()
+    : Minimum(
+          // get
+          [&]() -> const uint16_t & {
+              static uint16_t result;
+              result = (uint16_t)this->SendMessageW(PBM_GETRANGE, TRUE, 0);
+              return result;
+          },
+          // set
+          [&](const uint16_t &value) {
+              uint16_t maximum = this->Maximum;
+              this->SendMessageW(PBM_SETRANGE, 0, MAKELPARAM(value, maximum));
+          }),
+
+      Maximum(
+          // get
+          [&]() -> const uint16_t & {
+              static uint16_t result;
+              result = (uint16_t)this->SendMessageW(PBM_GETRANGE, FALSE, 0);
+              return result;
+          },
+          // set
+          [&](const uint16_t &value) {
+              uint16_t minimum = this->Minimum;
+              this->SendMessageW(PBM_SETRANGE, 0, MAKELPARAM(minimum, value));
+          }),
+
+      Value(
+          // get
+          [&]() -> const uint16_t & {
+              static uint16_t result;
+              result = (uint16_t)this->SendMessageW(PBM_GETPOS, 0, 0);
+              return result;
+          },
+          // set
+          [&](const uint16_t &value) {
+              this->SendMessageW(PBM_SETPOS, value, 0);
+          }),
+
+      State(
+          // get
+          [&]() -> const ProgressBarState & {
+              static ProgressBarState result;
+              result = (ProgressBarState)this->SendMessageW(PBM_GETSTATE, 0, 0);
+              return result;
+          },
+          // set
+          [&](const ProgressBarState &value) {
+              this->SendMessageW(PBM_SETSTATE, (WPARAM)value, 0);
+          })
 {
     this->InitControl(PROGRESS_CLASSW, L"", WS_CHILD | WS_VISIBLE | PBS_SMOOTH, 0);
-    this->Rect = sw::Rect(0, 0, 150, 24);
+    this->Rect = sw::Rect(0, 0, 150, 20);
 }
