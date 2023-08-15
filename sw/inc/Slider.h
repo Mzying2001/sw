808b9957dd1229fe6920dbed499f2adad074ae3e
Mzying2001 <Mzying2001@qq.com>
2023-08-16 00:47:06 +0800

    实现Slider

--- sw/inc/Slider.h
+++ sw/inc/Slider.h
@@ -12,6 +12,32 @@ namespace sw
     {
     public:
         /**
+         * @brief 最小值
+         */
+        const Property<int> Minimum;
+
+        /**
+         * @brief 最大值
+         */
+        const Property<int> Maximum;
+
+        /**
+         * @brief 当前滑块的值
+         */
+        const Property<int> Value;
+
+        /**
+         * @brief 是否显示为垂直滑块
+         */
+        const Property<bool> Vertical;
+
+        /**
+         * @brief 是否在滑动时显示数值提示
+         */
+        const Property<bool> ValueTooltips;
+
+    public:
+        /**
          * @brief 初始化滑块控件
          */
         Slider();
--- sw/src/Slider.cpp
+++ sw/src/Slider.cpp
@@ -1,7 +1,75 @@
 #include "Slider.h"
 
 sw::Slider::Slider()
+    : Minimum(
+          // get
+          [&]() -> const int & {
+              static int result;
+              result = (int)this->SendMessageW(TBM_GETRANGEMIN, 0, 0);
+              return result;
+          },
+          // set
+          [&](const int &value) {
+              this->SendMessageW(TBM_SETRANGEMIN, TRUE, value);
+          }),
+
+      Maximum(
+          // get
+          [&]() -> const int & {
+              static int result;
+              result = (int)this->SendMessageW(TBM_GETRANGEMAX, 0, 0);
+              return result;
+          },
+          // set
+          [&](const int &value) {
+              this->SendMessageW(TBM_SETRANGEMAX, TRUE, value);
+          }),
+
+      Value(
+          // get
+          [&]() -> const int & {
+              static int result;
+              result = (int)this->SendMessageW(TBM_GETPOS, 0, 0);
+              return result;
+          },
+          // set
+          [&](const int &value) {
+              this->SendMessageW(TBM_SETPOS, TRUE, value);
+          }),
+
+      Vertical(
+          // get
+          [&]() -> const bool & {
+              static bool result;
+              result = this->GetStyle(TBS_VERT);
+              return result;
+          },
+          // set
+          [&](const bool &value) {
+              this->SetStyle(TBS_VERT, value);
+          }),
+
+      ValueTooltips(
+          // get
+          [&]() -> const bool & {
+              static bool result;
+              result = this->GetStyle(TBS_TOOLTIPS);
+              return result;
+          },
+          // set
+          [&](const bool &value) {
+              if (this->ValueTooltips != value) {
+                  int maximum  = this->Maximum;
+                  int minimum  = this->Minimum;
+                  int position = this->Value;
+                  this->SetStyle(TBS_TOOLTIPS, value);
+                  this->ResetHandle();
+                  this->SendMessageW(TBM_SETRANGEMIN, FALSE, minimum);
+                  this->SendMessageW(TBM_SETRANGEMAX, FALSE, maximum);
+                  this->SendMessageW(TBM_SETPOS, TRUE, position);
+              }
+          })
 {
-    this->InitControl(TRACKBAR_CLASSW, L"", WS_CHILD | WS_VISIBLE, 0);
+    this->InitControl(TRACKBAR_CLASSW, L"", WS_CHILD | WS_VISIBLE | TBS_DOWNISLEFT, 0);
     this->Rect = sw::Rect(0, 0, 150, 30);
 }
