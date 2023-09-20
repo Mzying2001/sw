ccce2596ce5094fb2f388c7559416205a411ebca
Mzying2001 <Mzying2001@qq.com>
2023-09-20 23:19:49 +0800

    Update TabControl.h

--- sw/inc/TabControl.h
+++ sw/inc/TabControl.h
@@ -44,13 +44,13 @@ namespace sw
          * @brief               测量控件所需尺寸
          * @param availableSize 可用的尺寸
          */
-        virtual void Measure(const Size &availableSize);
+        virtual void Measure(const Size &availableSize) override;
 
         /**
          * @brief               安排控件位置
          * @param finalPosition 最终控件所安排的位置
          */
-        virtual void Arrange(const sw::Rect &finalPosition);
+        virtual void Arrange(const sw::Rect &finalPosition) override;
 
     protected:
         /**
