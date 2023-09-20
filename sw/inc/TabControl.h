391d73db18ed616cb3f3becbc55c766be218be93
Mzying2001 <Mzying2001@qq.com>
2023-09-20 22:07:07 +0800

    实现TabControl

--- sw/inc/TabControl.h
+++ sw/inc/TabControl.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Control.h"
+#include <CommCtrl.h>
 
 namespace sw
 {
@@ -8,8 +9,76 @@ namespace sw
     {
     public:
         /**
+         * @brief 内容区域位置与尺寸
+         */
+        const ReadOnlyProperty<sw::Rect> ContentRect;
+
+        /**
+         * @brief 当前页面的索引
+         */
+        const Property<int> SelectedIndex;
+
+    public:
+        /**
          * @brief 初始化TabControl
          */
         TabControl();
+
+        /**
+         * @brief 获取标签项的数量
+         */
+        int GetTabCount();
+
+        /**
+         * @brief 更新标签项信息
+         */
+        void UpdateTab();
+
+        /**
+         * @brief       更新指定索引处页面项的文本
+         * @param index 要更新的索引
+         */
+        void UpdateTabText(int index);
+
+        /**
+         * @brief               测量控件所需尺寸
+         * @param availableSize 可用的尺寸
+         */
+        virtual void Measure(const Size &availableSize);
+
+        /**
+         * @brief               安排控件位置
+         * @param finalPosition 最终控件所安排的位置
+         */
+        virtual void Arrange(const sw::Rect &finalPosition);
+
+    protected:
+        /**
+         * @brief         添加子元素后调用该函数
+         * @param element 添加的子元素
+         */
+        virtual void OnAddedChild(UIElement &element) override;
+
+        /**
+         * @brief         移除子元素后调用该函数
+         * @param element 移除的子元素
+         */
+        virtual void OnRemovedChild(UIElement &element) override;
+
+    private:
+        /**
+         * @brief 获取指定尺寸时的内容位置和尺寸
+         */
+        sw::Rect GetContentRect(const sw::Rect &rect);
+
+        /**
+         * @brief 根据选中的tab更新子元素的Visible属性
+         */
+        void UpdateChildVisible();
+
+        /**
+         * @brief 父窗口接收到WM_NOTIFY后调用发出通知控件的该函数
+         */
+        virtual void OnNotified(NMHDR *pNMHDR);
     };
 }
--- sw/src/TabControl.cpp
+++ sw/src/TabControl.cpp
@@ -1,6 +1,145 @@
 #include "TabControl.h"
+#include "Utils.h"
 
 sw::TabControl::TabControl()
+    : ContentRect(
+          // get
+          [&]() -> const sw::Rect & {
+              static sw::Rect result;
+              result = this->GetContentRect(this->ClientRect);
+              return result;
+          }),
+
+      SelectedIndex(
+          // get
+          [&]() -> const int & {
+              static int result;
+              result = (int)TabCtrl_GetCurSel(this->Handle);
+              return result;
+          },
+          // set
+          [&](const int &value) {
+              TabCtrl_SetCurSel(this->Handle, value);
+              this->UpdateChildVisible();
+          })
 {
-    // TODO
+    this->InitControl(WC_TABCONTROLW, L"", WS_CHILD | WS_VISIBLE | TCS_FOCUSNEVER, 0);
+    this->Rect = sw::Rect(0, 0, 200, 200);
+}
+
+int sw::TabControl::GetTabCount()
+{
+    return (int)TabCtrl_GetItemCount(this->Handle);
+}
+
+void sw::TabControl::UpdateTab()
+{
+    int childCount = this->ChildCount;
+    int tabCount   = this->GetTabCount();
+
+    HWND hwnd = this->Handle;
+
+    TCITEMW tci{};
+    tci.mask = TCIF_TEXT;
+
+    if (tabCount == childCount) {
+        for (int i = 0; i < childCount; ++i) {
+            tci.pszText = (LPWSTR)(*this)[i].Text->c_str();
+            TabCtrl_SetItem(hwnd, i, &tci);
+        }
+    } else {
+        int selectedIndex = this->SelectedIndex;
+        TabCtrl_DeleteAllItems(hwnd);
+        for (int i = 0; i < childCount; ++i) {
+            tci.pszText = (LPWSTR)(*this)[i].Text->c_str();
+            TabCtrl_InsertItem(hwnd, i, &tci);
+        }
+        if (selectedIndex >= 0) {
+            this->SelectedIndex = selectedIndex;
+        }
+    }
+}
+
+void sw::TabControl::UpdateTabText(int index)
+{
+    if (index < 0) {
+        return;
+    }
+
+    int childCount = this->ChildCount;
+    int tabCount   = this->GetTabCount();
+
+    if (index < childCount && index < tabCount) {
+        TCITEMW tci{};
+        tci.mask    = TCIF_TEXT;
+        tci.pszText = (LPWSTR)(*this)[index].Text->c_str();
+        TabCtrl_SetItem(this->Handle, index, &tci);
+    }
+}
+
+void sw::TabControl::Measure(const Size &availableSize)
+{
+    Size measureSize = availableSize;
+    this->UIElement::Measure(measureSize);
+
+    int selectedIndex = this->SelectedIndex;
+    if (selectedIndex < 0 || selectedIndex >= this->ChildCount) return;
+
+    sw::Rect contentRect;
+    contentRect = this->GetContentRect(contentRect);
+
+    measureSize = {contentRect.width, contentRect.height};
+    (*this)[selectedIndex].Measure(measureSize);
+}
+
+void sw::TabControl::Arrange(const sw::Rect &finalPosition)
+{
+    this->UIElement::Arrange(finalPosition);
+
+    int selectedIndex = this->SelectedIndex;
+    if (selectedIndex < 0 || selectedIndex >= this->ChildCount) return;
+
+    (*this)[selectedIndex].Arrange(this->ContentRect);
+}
+
+void sw::TabControl::OnAddedChild(UIElement &element)
+{
+    TCITEMW tci{};
+    tci.mask    = TCIF_TEXT;
+    tci.pszText = (LPWSTR)L"";
+
+    TabCtrl_InsertItem(this->Handle, this->GetTabCount(), &tci);
+    this->UpdateTab();
+}
+
+void sw::TabControl::OnRemovedChild(UIElement &element)
+{
+    TabCtrl_DeleteItem(this->Handle, this->GetTabCount() - 1);
+    this->UpdateTab();
+}
+
+sw::Rect sw::TabControl::GetContentRect(const sw::Rect &rect)
+{
+    RECT rt = rect;
+    this->SendMessageW(TCM_ADJUSTRECT, FALSE, reinterpret_cast<LPARAM>(&rt));
+    return rt;
+}
+
+void sw::TabControl::UpdateChildVisible()
+{
+    int selectedIndex = this->SelectedIndex;
+    int childCount    = this->ChildCount;
+
+    for (int i = 0; i < childCount; ++i) {
+        UIElement &item = (*this)[i];
+        item.Visible    = i == selectedIndex;
+        item.Visible    = i == selectedIndex; // 不加这个在点击按钮后立刻换页按钮会莫名其妙固定在界面上
+    }
+}
+
+void sw::TabControl::OnNotified(NMHDR *pNMHDR)
+{
+    if (pNMHDR->code == TCN_SELCHANGE) {
+        this->UpdateChildVisible();
+    }
 }
