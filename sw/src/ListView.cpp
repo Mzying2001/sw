e7c533d9628b13b7991d4d2bd6279ad518cb4f87
Mzying2001 <Mzying2001@qq.com>
2023-10-29 20:29:43 +0800

    让ListView继承自ItemsControl<StrList>

--- sw/src/ListView.cpp
+++ sw/src/ListView.cpp
@@ -7,4 +7,60 @@ sw::ListView::ListView()
     this->TabStop = true;
 }
 
-// TODO:
+int sw::ListView::GetItemsCount()
+{
+    // TODO
+    return 0;
+}
+
+int sw::ListView::GetSelectedIndex()
+{
+    // TODO
+    return 0;
+}
+
+void sw::ListView::SetSelectedIndex(int index)
+{
+    // TODO
+}
+
+sw::StrList sw::ListView::GetSelectedItem()
+{
+    // TODO
+    return StrList();
+}
+
+void sw::ListView::Clear()
+{
+    // TODO
+}
+
+sw::StrList sw::ListView::GetItemAt(int index)
+{
+    // TODO
+    return StrList();
+}
+
+bool sw::ListView::AddItem(const StrList &item)
+{
+    // TODO
+    return false;
+}
+
+bool sw::ListView::InsertItem(int index, const StrList &item)
+{
+    // TODO
+    return false;
+}
+
+bool sw::ListView::UpdateItem(int index, const StrList &newValue)
+{
+    // TODO
+    return false;
+}
+
+bool sw::ListView::RemoveItemAt(int index)
+{
+    // TODO
+    return false;
+}
