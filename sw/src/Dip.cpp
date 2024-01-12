e9789f0273c122b83f52d33c8708a11240929560
Mzying2001 <Mzying2001@qq.com>
2024-01-12 16:26:52 +0800

    添加转换函数

--- sw/src/Dip.cpp
+++ sw/src/Dip.cpp
@@ -1,36 +1,60 @@
 #include "Dip.h"
+#include <cmath>
 
 #if !defined(USER_DEFAULT_SCREEN_DPI)
 #define USER_DEFAULT_SCREEN_DPI 96
 #endif
 
-static sw::Dip::DipScaleInfo _GetScaleInfo();
-static sw::Dip::DipScaleInfo _dipScaleInfo = _GetScaleInfo();
+struct _ScaleInfo {
+    double scaleX;
+    double scaleY;
+    _ScaleInfo();
+} _scaleInfo;
 
-void sw::Dip::Update(int dpiX, int dpiY)
+_ScaleInfo::_ScaleInfo()
 {
-    _dipScaleInfo.scaleX = (double)USER_DEFAULT_SCREEN_DPI / dpiX;
-    _dipScaleInfo.scaleY = (double)USER_DEFAULT_SCREEN_DPI / dpiY;
+    HDC hdc      = GetDC(NULL);
+    this->scaleX = (double)USER_DEFAULT_SCREEN_DPI / GetDeviceCaps(hdc, LOGPIXELSX);
+    this->scaleY = (double)USER_DEFAULT_SCREEN_DPI / GetDeviceCaps(hdc, LOGPIXELSY);
+    ReleaseDC(NULL, hdc);
 }
+
+/*================================================================================*/
 
 const sw::ReadOnlyProperty<double> sw::Dip::ScaleX(
     []() -> const double & {
-        return _dipScaleInfo.scaleX;
+        return _scaleInfo.scaleX;
     } //
 );
 
 const sw::ReadOnlyProperty<double> sw::Dip::ScaleY(
     []() -> const double & {
-        return _dipScaleInfo.scaleY;
+        return _scaleInfo.scaleY;
     } //
 );
 
-static sw::Dip::DipScaleInfo _GetScaleInfo()
+void sw::Dip::Update(int dpiX, int dpiY)
 {
-    sw::Dip::DipScaleInfo info{};
-    HDC hdc     = GetDC(NULL);
-    info.scaleX = (double)USER_DEFAULT_SCREEN_DPI / GetDeviceCaps(hdc, LOGPIXELSX);
-    info.scaleY = (double)USER_DEFAULT_SCREEN_DPI / GetDeviceCaps(hdc, LOGPIXELSY);
-    ReleaseDC(NULL, hdc);
-    return info;
+    _scaleInfo.scaleX = (double)USER_DEFAULT_SCREEN_DPI / dpiX;
+    _scaleInfo.scaleY = (double)USER_DEFAULT_SCREEN_DPI / dpiY;
+}
+
+double sw::Dip::PxToDipX(int px)
+{
+    return px * _scaleInfo.scaleX;
+}
+
+double sw::Dip::PxToDipY(int px)
+{
+    return px * _scaleInfo.scaleY;
+}
+
+int sw::Dip::DipToPxX(double dip)
+{
+    return (int)std::lround(dip / _scaleInfo.scaleX);
+}
+
+int sw::Dip::DipToPxY(double dip)
+{
+    return (int)std::lround(dip / _scaleInfo.scaleY);
 }
