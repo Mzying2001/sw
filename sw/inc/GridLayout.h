265c5b7032e83fb5fa842b425fc0c6e40a98762a
Mzying2001 <Mzying2001@qq.com>
2024-01-14 16:05:44 +0800

    添加GridLayoutTag

--- sw/inc/GridLayout.h
+++ sw/inc/GridLayout.h
@@ -4,8 +4,52 @@
 
 namespace sw
 {
+    /**
+     * @brief Grid布局方式的布局标记
+     */
+    struct GridLayoutTag {
+        /**
+         * @brief 所在行
+         */
+        uint16_t row;
+
+        /**
+         * @brief 所在列
+         */
+        uint16_t column;
+
+        /**
+         * @brief 所跨行数
+         */
+        uint16_t rowSpan;
+
+        /**
+         * @brief 所跨列数
+         */
+        uint16_t columnSpan;
+
+        /**
+         * @brief 初始化GridLayoutTag
+         */
+        GridLayoutTag(uint16_t row, uint16_t column, uint16_t rowSpan, uint16_t columnSpan);
+
+        /**
+         * @brief 从LayoutTag创建
+         */
+        GridLayoutTag(uint64_t layoutTag);
+
+        /**
+         * @brief 隐式转换LayoutTag
+         */
+        operator uint64_t() const;
+    };
+
+    /**
+     * @brief Grid布局方式
+     */
     class GridLayout : public LayoutHost
     {
+    public:
         /**
          * @brief 计算所需尺寸
          */
--- sw/src/GridLayout.cpp
+++ sw/src/GridLayout.cpp
@@ -1,5 +1,21 @@
 #include "GridLayout.h"
 
+sw::GridLayoutTag::GridLayoutTag(uint16_t row, uint16_t column, uint16_t rowSpan, uint16_t columnSpan)
+    : row(row), column(column), rowSpan(rowSpan), columnSpan(columnSpan)
+{
+}
+
+sw::GridLayoutTag::GridLayoutTag(uint64_t layoutTag)
+    : row((layoutTag >> 0) & 0xffff), column((layoutTag >> 16) & 0xffff),
+      rowSpan((layoutTag >> 32) & 0xffff), columnSpan((layoutTag >> 48) & 0xffff)
+{
+}
+
+sw::GridLayoutTag::operator uint64_t() const
+{
+    return *reinterpret_cast<const uint64_t *>(this);
+}
+
 void sw::GridLayout::MeasureOverride(Size &availableSize)
 {
     // TODO
