38225ca2ade76523cf8f7d02a9cf8cf6f691a283
Mzying2001 <Mzying2001@qq.com>
2024-01-19 00:42:33 +0800

    Update GridLayout.cpp

--- sw/src/GridLayout.cpp
+++ sw/src/GridLayout.cpp
@@ -1,6 +1,7 @@
 #include "GridLayout.h"
 #include "Utils.h"
 #include <algorithm>
+#include <cmath>
 
 sw::GridLayoutTag::GridLayoutTag()
     : row(0), column(0), rowSpan(1), columnSpan(1)
@@ -65,11 +66,186 @@ void sw::GridLayout::MeasureOverride(Size &availableSize)
     int colCount   = (int)this->_internalData.colsInfo.size();
     int childCount = (int)this->_internalData.childrenInfo.size();
 
-    // TODO
+    bool widthSizeToContent  = std::isinf(availableSize.width);
+    bool heightSizeToContent = std::isinf(availableSize.height);
 
     // Measure列
 
+    // 对childrenInfo进行排序
+    // 优先级 FixSize > AutoSize > FillRemain
+    // 对于优先级相同的则按照columnSpan从小到大排序
+    std::sort(
+        this->_internalData.childrenInfo.begin(),
+        this->_internalData.childrenInfo.end(),
+        [](const _ChildInfo &a, const _ChildInfo &b) -> bool {
+            if (a.colMeasureType != b.colMeasureType) {
+                // 先FixSize，后AutoSize，最后FillRemain
+                return a.colMeasureType < b.colMeasureType;
+            } else {
+                // 若类型相同，则按照跨列数从小到大measure
+                return a.layoutTag.columnSpan < b.layoutTag.columnSpan;
+            }
+        });
+
+    for (_ChildInfo &childInfo : this->_internalData.childrenInfo) {
+        bool breakFlag = false; // 标记是否退出循环
+
+        switch (childInfo.colMeasureType) {
+            case GridRCType::FixSize: {
+                // 第一次measure目的是确定列宽度，因此FixSize列的元素没必要在此时measure
+                break;
+            }
+
+            case GridRCType::AutoSize: {
+                childInfo.instance->Measure(Size(INFINITY, INFINITY));
+                Size childDesireSize = childInfo.instance->GetDesireSize();
+
+                if (childInfo.layoutTag.columnSpan <= 1) {
+                    // 若ColumnSpan为1，直接更新对应列的尺寸
+                    this->_internalData.colsInfo[childInfo.layoutTag.column].size =
+                        Utils::Max(this->_internalData.colsInfo[childInfo.layoutTag.column].size, childDesireSize.width);
+                } else {
+                    // 若ColumnSpan不为1，则对所跨类型为AutoSize的列均匀贡献
+                    int autoSizeColCount = 0;
+                    double totalWidth    = 0;
+
+                    for (int i = 0; i < childInfo.layoutTag.columnSpan; ++i) {
+                        _ColInfo &colInfo =
+                            this->_internalData.colsInfo[childInfo.layoutTag.column + i];
+                        autoSizeColCount += colInfo.col.type == GridRCType::AutoSize;
+                        totalWidth += colInfo.size;
+                    }
+
+                    if (totalWidth < childDesireSize.width) {
+                        double tmp =
+                            (childDesireSize.width - totalWidth) / autoSizeColCount;
+                        for (int i = 0; i < childInfo.layoutTag.columnSpan; ++i) {
+                            _ColInfo &colInfo =
+                                this->_internalData.colsInfo[childInfo.layoutTag.column + i];
+                            if (colInfo.col.type == GridRCType::AutoSize) {
+                                colInfo.size += tmp;
+                            }
+                        }
+                    }
+                }
+                break;
+            }
+
+            case GridRCType::FillRemain: {
+                if (!widthSizeToContent) {
+                    // 若宽度不是由内容确定（availableSize.width为inf），则类型为FillRemain的列宽与内容无关
+                    // 此时可以直接根据剩余的大小计算，直接退出循环
+                    breakFlag = true;
+                    break;
+                }
+
+                // 宽度由内容决定，对元素进行measure，以占空间最大的元素为基准分配大小
+                childInfo.instance->Measure(Size(INFINITY, INFINITY));
+                Size childDesireSize = childInfo.instance->GetDesireSize();
+
+                if (childInfo.layoutTag.columnSpan <= 1) {
+                    // 若ColumnSpan为1，直接更新对应列的尺寸
+                    this->_internalData.colsInfo[childInfo.layoutTag.column].size =
+                        Utils::Max(this->_internalData.colsInfo[childInfo.layoutTag.column].size, childDesireSize.width);
+                } else {
+                    // 若ColumnSpan不为1，则对所跨类型为FillRemain的列均匀贡献
+                    int autoSizeColCount = 0;
+                    double totalWidth    = 0;
+
+                    for (int i = 0; i < childInfo.layoutTag.columnSpan; ++i) {
+                        _ColInfo &colInfo =
+                            this->_internalData.colsInfo[childInfo.layoutTag.column + i];
+                        autoSizeColCount += colInfo.col.type == GridRCType::FillRemain;
+                        totalWidth += colInfo.size;
+                    }
+
+                    if (totalWidth < childDesireSize.width) {
+                        double tmp =
+                            (childDesireSize.width - totalWidth) / autoSizeColCount;
+                        for (int i = 0; i < childInfo.layoutTag.columnSpan; ++i) {
+                            _ColInfo &colInfo =
+                                this->_internalData.colsInfo[childInfo.layoutTag.column + i];
+                            if (colInfo.col.type == GridRCType::FillRemain) {
+                                colInfo.size += tmp;
+                            }
+                        }
+                    }
+                }
+                break;
+            }
+        }
+
+        if (breakFlag) {
+            break;
+        }
+    }
+
+    if (!widthSizeToContent) {
+        // 宽度不是由内容决定时计算类型为FillRemain的列的宽度
+        int count          = 0;
+        double widthSum    = 0;
+        double remainWidth = availableSize.width;
+        for (_ColInfo &colInfo : this->_internalData.colsInfo) {
+            if (colInfo.col.type == GridRCType::FillRemain) {
+                ++count;
+                widthSum += colInfo.col.width;
+            } else {
+                remainWidth -= colInfo.size;
+            }
+        }
+        if (count && widthSum > 0) {
+            if (remainWidth < 0) {
+                remainWidth = 0;
+            }
+            for (_ColInfo &colInfo : this->_internalData.colsInfo) {
+                if (colInfo.col.type == GridRCType::FillRemain) {
+                    colInfo.size = remainWidth * colInfo.col.width / widthSum;
+                }
+            }
+        }
+    } else {
+        // 宽度由内容决定，依据所占宽度最大元素为基准计算列宽度
+        int count             = 0;
+        double widthSum       = 0;
+        _ColInfo *maxWidthCol = nullptr;
+        for (_ColInfo &colInfo : this->_internalData.colsInfo) {
+            if (colInfo.col.type == GridRCType::FillRemain &&
+                (maxWidthCol == nullptr || colInfo.size > maxWidthCol->size)) {
+                ++count;
+                widthSum += colInfo.col.width;
+                maxWidthCol = &colInfo;
+            }
+        }
+        if (count > 1 && widthSum > 0) {
+            double tmp =
+                maxWidthCol->size / (maxWidthCol->col.width / widthSum);
+            for (_ColInfo &colInfo : this->_internalData.colsInfo) {
+                if (colInfo.col.type == GridRCType::FillRemain) {
+                    colInfo.size = tmp * colInfo.col.width / widthSum;
+                }
+            }
+        }
+    }
+
     // Measure行
+
+    // 对childrenInfo进行排序
+    // 优先级 FixSize > AutoSize > FillRemain
+    // 对于优先级相同的则按照rowSpan从小到大排序
+    std::sort(
+        this->_internalData.childrenInfo.begin(),
+        this->_internalData.childrenInfo.end(),
+        [](const _ChildInfo &a, const _ChildInfo &b) -> bool {
+            if (a.rowMeasureType != b.rowMeasureType) {
+                // 先FixSize，后AutoSize，最后FillRemain
+                return a.rowMeasureType < b.rowMeasureType;
+            } else {
+                // 若类型相同，则按照跨列数从小到大measure
+                return a.layoutTag.rowSpan < b.layoutTag.rowSpan;
+            }
+        });
+
+    // TODO
 
     // 设置DesireSize
     this->SetDesireSize(desireSize);
@@ -95,8 +271,17 @@ void sw::GridLayout::_UpdateInternalData()
         } else {
             for (GridRow &row : this->rows) {
                 info.row = row;
-                if (info.row.height < 0)
+                // 防止值小于0
+                if (info.row.height < 0) {
                     info.row.height = 0;
+                }
+                if (info.row.type == GridRCType::FixSize) {
+                    // 若类型为FixSize，此时即可确定行高
+                    info.size = info.row.height;
+                } else {
+                    // 其他类型此时无法确定行高
+                    info.size = 0;
+                }
                 this->_internalData.rowsInfo.emplace_back(info);
             }
         }
@@ -111,8 +296,17 @@ void sw::GridLayout::_UpdateInternalData()
         } else {
             for (GridColumn &col : this->columns) {
                 info.col = col;
-                if (info.col.width < 0)
+                // 防止值小于0
+                if (info.col.width < 0) {
                     info.col.width = 0;
+                }
+                if (info.col.type == GridRCType::FixSize) {
+                    // 若类型为FixSize，此时即可确定列宽
+                    info.size = info.col.width;
+                } else {
+                    // 其他类型此时无法确定列宽
+                    info.size = 0;
+                }
                 this->_internalData.colsInfo.emplace_back(info);
             }
         }
