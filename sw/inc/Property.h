6aca8177366aecd2371b5bed89e5fd7b54d210da
Mzying2001 <Mzying2001@qq.com>
2024-01-19 18:42:41 +0800

    Update Property.h

--- sw/inc/Property.h
+++ sw/inc/Property.h
@@ -5,110 +5,128 @@
 
 namespace sw
 {
-    template <class T>
+    /**
+     * @brief 只读属性
+     */
+    template <typename T>
     class ReadOnlyProperty
     {
     private:
+        /**
+         * @brief 读取属性的函数
+         */
         std::function<const T &()> _funcGet;
 
     public:
-        ReadOnlyProperty(const std::function<const T &()> &funcGet);
-        const T &Get() const;
-        operator const T &() const;
-        const T *operator->() const;
+        /**
+         * @brief 初始化ReadOnlyProperty
+         */
+        ReadOnlyProperty(const std::function<const T &()> &funcGet)
+            : _funcGet(funcGet)
+        {
+        }
+
+        /**
+         * @brief 读属性
+         */
+        const T &Get() const
+        {
+            return this->_funcGet();
+        }
+
+        /**
+         * @brief 读属性
+         */
+        operator const T &() const
+        {
+            return this->_funcGet();
+        }
+
+        /**
+         * @brief 取属性成员
+         */
+        const T *operator->() const
+        {
+            return &this->_funcGet();
+        }
     };
 
-    template <class T>
+    /**
+     * @brief 只写属性
+     */
+    template <typename T>
     class WriteOnlyProperty
     {
     private:
+        /**
+         * @brief 写属性的函数
+         */
         std::function<void(const T &)> _funcSet;
 
     public:
-        WriteOnlyProperty(const std::function<void(const T &)> &funcSet);
-        void Set(const T &value) const;
-        const WriteOnlyProperty &operator=(const T &value) const;
+        /**
+         * @brief 初始化WriteOnlyProperty
+         */
+        WriteOnlyProperty(const std::function<void(const T &)> &funcSet)
+            : _funcSet(funcSet)
+        {
+        }
+
+        /**
+         * @brief 写属性
+         */
+        void Set(const T &value) const
+        {
+            this->_funcSet(value);
+        }
+
+        /**
+         * @brief 写属性
+         */
+        const WriteOnlyProperty &operator=(const T &value) const
+        {
+            this->_funcSet(value);
+            return *this;
+        }
     };
 
-    template <class T>
-    class Property : public ReadOnlyProperty<T>,
-                     public WriteOnlyProperty<T>
+    /**
+     * @brief 属性
+     */
+    template <typename T>
+    class Property : public ReadOnlyProperty<T>, public WriteOnlyProperty<T>
     {
     public:
-        Property(const std::function<const T &()> &funcGet, const std::function<void(const T &)> &funcSet);
-        const Property &operator=(const T &value) const;
-        T *operator->() const;
-    };
+        /**
+         * @brief 初始化Property
+         */
+        Property(const std::function<const T &()> &funcGet, const std::function<void(const T &)> &funcSet)
+            : ReadOnlyProperty<T>(funcGet), WriteOnlyProperty<T>(funcSet)
+        {
+        }
 
-    template <class T>
-    std::wostream &operator<<(std::wostream &wos, const ReadOnlyProperty<T> &prop);
+        /**
+         * @brief 写属性
+         */
+        const Property &operator=(const T &value) const
+        {
+            this->Set(value);
+            return *this;
+        }
+
+        /**
+         * @brief 取属性成员
+         */
+        T *operator->() const
+        {
+            const T &value = this->Get();
+            return const_cast<T *>(&value);
+        }
+    };
 
     /*================================================================================*/
 
-    template <class T>
-    inline ReadOnlyProperty<T>::ReadOnlyProperty(const std::function<const T &()> &funcGet)
-        : _funcGet(funcGet)
-    {
-    }
-
-    template <class T>
-    inline const T &ReadOnlyProperty<T>::Get() const
-    {
-        return this->_funcGet();
-    }
-
-    template <class T>
-    inline ReadOnlyProperty<T>::operator const T &() const
-    {
-        return this->_funcGet();
-    }
-
-    template <class T>
-    inline const T *ReadOnlyProperty<T>::operator->() const
-    {
-        return &this->_funcGet();
-    }
-
-    template <class T>
-    inline WriteOnlyProperty<T>::WriteOnlyProperty(const std::function<void(const T &)> &funcSet)
-        : _funcSet(funcSet)
-    {
-    }
-
-    template <class T>
-    inline void WriteOnlyProperty<T>::Set(const T &value) const
-    {
-        this->_funcSet(value);
-    }
-
-    template <class T>
-    inline const WriteOnlyProperty<T> &WriteOnlyProperty<T>::operator=(const T &value) const
-    {
-        this->_funcSet(value);
-        return *this;
-    }
-
-    template <class T>
-    inline Property<T>::Property(const std::function<const T &()> &funcGet, const std::function<void(const T &)> &funcSet)
-        : ReadOnlyProperty<T>(funcGet), WriteOnlyProperty<T>(funcSet)
-    {
-    }
-
-    template <class T>
-    inline const Property<T> &Property<T>::operator=(const T &value) const
-    {
-        this->Set(value);
-        return *this;
-    }
-
-    template <class T>
-    inline T *Property<T>::operator->() const
-    {
-        const T &value = this->Get();
-        return const_cast<T *>(&value);
-    }
-
-    template <class T>
+    template <typename T>
     inline std::wostream &operator<<(std::wostream &wos, const ReadOnlyProperty<T> &prop)
     {
         return wos << prop.Get();
