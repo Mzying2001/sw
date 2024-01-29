e029bf25692df0160bb47ccd97fe3ee9dfeef919
Mzying2001 <Mzying2001@qq.com>
2024-01-29 19:08:14 +0800

    添加Notify属性

--- sw/src/StaticControl.cpp
+++ sw/src/StaticControl.cpp
@@ -1,6 +1,17 @@
 #include "StaticControl.h"
 
 sw::StaticControl::StaticControl()
+    : Notify(
+          // get
+          [&]() -> const bool & {
+              static bool result;
+              result = this->GetStyle(SS_NOTIFY);
+              return result;
+          },
+          // set
+          [&](const bool &value) {
+              this->SetStyle(SS_NOTIFY, value);
+          })
 {
     this->InitControl(L"STATIC", L"", WS_CHILD | WS_VISIBLE | WS_CLIPSIBLINGS, 0);
 }
