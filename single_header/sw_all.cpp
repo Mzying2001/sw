0dd2dcacd7b9cae305cac1555661f34bcc99c366
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2024-04-01 00:36:07 +0800

    Update sw_all

--- single_header/sw_all.cpp
+++ single_header/sw_all.cpp
@@ -2147,6 +2147,54 @@ sw::GroupBox::GroupBox()
     this->InheritTextColor = true;
 }
 
+// HotKeyControl.cpp
+
+sw::HotKeyControl::HotKeyControl()
+    : Value(
+          // get
+          [&]() -> const HotKey & {
+              return this->_value;
+          },
+          // set
+          [&](const HotKey &value) {
+              if (value.key != this->_value.key && value.modifier != this->_value.modifier) {
+                  WORD val = MAKEWORD(value.key, value.modifier);
+                  this->SendMessageW(HKM_SETHOTKEY, val, 0);
+                  this->_UpdateValue();
+                  this->OnValueChanged(this->_value);
+              }
+          })
+{
+    this->InitControl(HOTKEY_CLASSW, L"", WS_CHILD | WS_VISIBLE | WS_CLIPSIBLINGS, 0);
+    this->_UpdateValue();
+    this->Rect = {0, 0, 100, 24};
+}
+
+void sw::HotKeyControl::SetRules(HotKeyCombination invalidComb, HotKeyModifier defaultModifier)
+{
+    this->SendMessageW(HKM_SETRULES, static_cast<WPARAM>(invalidComb), static_cast<LPARAM>(defaultModifier));
+}
+
+void sw::HotKeyControl::OnCommand(int code)
+{
+    if (code == EN_CHANGE) {
+        this->_UpdateValue();
+        this->OnValueChanged(this->_value);
+    }
+}
+
+void sw::HotKeyControl::OnValueChanged(HotKey value)
+{
+    HotKeyValueChangedEventArgs arg{value.key, value.modifier};
+    this->RaiseRoutedEvent(arg);
+}
+
+void sw::HotKeyControl::_UpdateValue()
+{
+    WORD val     = LOWORD(this->SendMessageW(HKM_GETHOTKEY, 0, 0));
+    this->_value = {/*key*/ VirtualKey(LOBYTE(val)), /*modifier*/ HotKeyModifier(HIBYTE(val))};
+}
+
 // HwndHost.cpp
 
 sw::HwndHost::HwndHost()
@@ -2293,6 +2341,267 @@ HICON sw::IconBox::_SetIconIfNotNull(HICON hIcon)
         this->_SetIcon(hIcon);
     }
     return hIcon;
+}
+
+// ImageList.cpp
+
+sw::ImageList::ImageList(HIMAGELIST hImageList, bool isWrap)
+    : _hImageList(hImageList), _isWrap(isWrap)
+{
+}
+
+sw::ImageList::ImageList(int cx, int cy, UINT flags, int cInitial, int cGrow)
+    : ImageList(ImageList_Create(cx, cy, flags, cInitial, cGrow), false)
+{
+}
+
+sw::ImageList::ImageList(const ImageList &value)
+{
+    if (value._isWrap) {
+        this->_isWrap     = true;
+        this->_hImageList = value._hImageList;
+    } else {
+        this->_isWrap     = false;
+        this->_hImageList = ImageList_Duplicate(value._hImageList);
+    }
+}
+
+sw::ImageList::ImageList(ImageList &&rvalue)
+{
+    this->_isWrap      = rvalue._isWrap;
+    this->_hImageList  = rvalue._hImageList;
+    rvalue._hImageList = NULL;
+}
+
+sw::ImageList::~ImageList()
+{
+    this->_DestroyIfNotWrap();
+}
+
+sw::ImageList &sw::ImageList::operator=(const ImageList &value)
+{
+    this->_DestroyIfNotWrap();
+
+    if (value._isWrap) {
+        this->_isWrap     = true;
+        this->_hImageList = value._hImageList;
+    } else {
+        this->_isWrap     = false;
+        this->_hImageList = ImageList_Duplicate(value._hImageList);
+    }
+
+    return *this;
+}
+
+sw::ImageList &sw::ImageList::operator=(ImageList &&rvalue)
+{
+    this->_DestroyIfNotWrap();
+
+    this->_isWrap      = rvalue._isWrap;
+    this->_hImageList  = rvalue._hImageList;
+    rvalue._hImageList = NULL;
+
+    return *this;
+}
+
+sw::ImageList sw::ImageList::Create(int cx, int cy, UINT flags, int cInitial, int cGrow)
+{
+    return ImageList{cx, cy, flags, cInitial, cGrow};
+}
+
+sw::ImageList sw::ImageList::Wrap(HIMAGELIST hImageList)
+{
+    return ImageList{hImageList, true};
+}
+
+bool sw::ImageList::Copy(const ImageList &dst, int iDst, const ImageList &src, int iSrc, UINT uFlags)
+{
+    return ImageList_Copy(dst._hImageList, iDst, src._hImageList, iSrc, uFlags);
+}
+
+bool sw::ImageList::DragEnter(HWND hwndLock, double x, double y)
+{
+    return ImageList_DragEnter(hwndLock, Dip::DipToPxX(x), Dip::DipToPxY(y));
+}
+
+bool sw::ImageList::DragLeave(HWND hwndLock)
+{
+    return ImageList_DragLeave(hwndLock);
+}
+
+bool sw::ImageList::DragMove(double x, double y)
+{
+    return ImageList_DragMove(Dip::DipToPxX(x), Dip::DipToPxY(y));
+}
+
+bool sw::ImageList::DragShowNolock(bool fShow)
+{
+    return ImageList_DragShowNolock(fShow);
+}
+
+void sw::ImageList::EndDrag()
+{
+    ImageList_EndDrag();
+}
+
+sw::ImageList sw::ImageList::GetDragImage(Point &pt, Point &ptHotspot)
+{
+    POINT points[2];
+    HIMAGELIST h = ImageList_GetDragImage(&points[0], &points[1]);
+    pt = points[0], ptHotspot = points[1];
+    return ImageList{h, false};
+}
+
+sw::ImageList sw::ImageList::LoadImageA(HINSTANCE hi, LPCSTR lpbmp, int cx, int cGrow, COLORREF crMask, UINT uType, UINT uFlags)
+{
+    return ImageList{ImageList_LoadImageA(hi, lpbmp, cx, cGrow, crMask, uType, uFlags), false};
+}
+
+sw::ImageList sw::ImageList::LoadImageW(HINSTANCE hi, LPCWSTR lpbmp, int cx, int cGrow, COLORREF crMask, UINT uType, UINT uFlags)
+{
+    return ImageList{ImageList_LoadImageW(hi, lpbmp, cx, cGrow, crMask, uType, uFlags), false};
+}
+
+sw::ImageList sw::ImageList::Merge(const ImageList &iml1, int i1, const ImageList &iml2, int i2, int dx, int dy)
+{
+    return ImageList{ImageList_Merge(iml1._hImageList, i1, iml2._hImageList, i2, dx, dy), false};
+}
+
+sw::ImageList sw::ImageList::Read(IStream *pstm)
+{
+    return ImageList{ImageList_Read(pstm), false};
+}
+
+HIMAGELIST sw::ImageList::GetHandle()
+{
+    return this->_hImageList;
+}
+
+bool sw::ImageList::IsWrap()
+{
+    return this->_isWrap;
+}
+
+int sw::ImageList::Add(HBITMAP hbmImage, HBITMAP hbmMask)
+{
+    return ImageList_Add(this->_hImageList, hbmImage, hbmMask);
+}
+
+int sw::ImageList::AddMasked(HBITMAP hbmImage, COLORREF crMask)
+{
+    return ImageList_AddMasked(this->_hImageList, hbmImage, crMask);
+}
+
+bool sw::ImageList::BeginDrag(int iTrack, double dxHotspot, double dyHotspot)
+{
+    return ImageList_BeginDrag(this->_hImageList, iTrack, Dip::DipToPxX(dxHotspot), Dip::DipToPxY(dyHotspot));
+}
+
+bool sw::ImageList::Draw(int i, HDC hdcDst, double x, double y, UINT fStyle)
+{
+    return this->DrawPx(i, hdcDst, Dip::DipToPxX(x), Dip::DipToPxY(y), fStyle);
+}
+
+bool sw::ImageList::Draw(int i, HDC hdcDst, double x, double y, double dx, double dy, COLORREF rgbBk, COLORREF rgbFg, UINT fStyle)
+{
+    return this->DrawPx(i, hdcDst, Dip::DipToPxX(x), Dip::DipToPxY(y), Dip::DipToPxX(dx), Dip::DipToPxY(dy), rgbBk, rgbFg, fStyle);
+}
+
+bool sw::ImageList::DrawPx(int i, HDC hdcDst, int x, int y, UINT fStyle)
+{
+    return ImageList_Draw(this->_hImageList, i, hdcDst, x, y, fStyle);
+}
+
+bool sw::ImageList::DrawPx(int i, HDC hdcDst, int x, int y, int dx, int dy, COLORREF rgbBk, COLORREF rgbFg, UINT fStyle)
+{
+    return ImageList_DrawEx(this->_hImageList, i, hdcDst, x, y, dx, dy, rgbBk, rgbFg, fStyle);
+}
+
+sw::ImageList sw::ImageList::Duplicate()
+{
+    return ImageList{ImageList_Duplicate(this->_hImageList), false};
+}
+
+COLORREF sw::ImageList::GetBkColor()
+{
+    return ImageList_GetBkColor(this->_hImageList);
+}
+
+HICON sw::ImageList::GetIcon(int i, UINT flags)
+{
+    return ImageList_GetIcon(this->_hImageList, i, flags);
+}
+
+bool sw::ImageList::GetIconSize(int &cx, int &cy)
+{
+    return ImageList_GetIconSize(this->_hImageList, &cx, &cy);
+}
+
+int sw::ImageList::GetImageCount()
+{
+    return ImageList_GetImageCount(this->_hImageList);
+}
+
+bool sw::ImageList::GetImageInfo(int i, IMAGEINFO *pImageInfo)
+{
+    return ImageList_GetImageInfo(this->_hImageList, i, pImageInfo);
+}
+
+bool sw::ImageList::Remove(int i)
+{
+    return ImageList_Remove(this->_hImageList, i);
+}
+
+bool sw::ImageList::RemoveAll()
+{
+    return this->Remove(-1);
+}
+
+bool sw::ImageList::Replace(int i, HBITMAP hbmImage, HBITMAP hbmMask)
+{
+    return ImageList_Replace(this->_hImageList, i, hbmImage, hbmMask);
+}
+
+int sw::ImageList::ReplaceIcon(int i, HICON hicon)
+{
+    return ImageList_ReplaceIcon(this->_hImageList, i, hicon);
+}
+
+COLORREF sw::ImageList::SetBkColor(COLORREF clrBk)
+{
+    return ImageList_SetBkColor(this->_hImageList, clrBk);
+}
+
+bool sw::ImageList::SetDragCursorImage(int iDrag, int dxHotspot, int dyHotspot)
+{
+    return ImageList_SetDragCursorImage(this->_hImageList, iDrag, dxHotspot, dyHotspot);
+}
+
+bool sw::ImageList::SetIconSize(int cx, int cy)
+{
+    return ImageList_SetIconSize(this->_hImageList, cx, cy);
+}
+
+bool sw::ImageList::SetImageCount(UINT uNewCount)
+{
+    return ImageList_SetImageCount(this->_hImageList, uNewCount);
+}
+
+bool sw::ImageList::SetOverlayImage(int iImage, int iOverlay)
+{
+    return ImageList_SetOverlayImage(this->_hImageList, iImage, iOverlay);
+}
+
+bool sw::ImageList::Write(IStream *pstm)
+{
+    return ImageList_Write(this->_hImageList, pstm);
+}
+
+void sw::ImageList::_DestroyIfNotWrap()
+{
+    if (this->_hImageList != NULL && !this->_isWrap) {
+        ImageList_Destroy(this->_hImageList);
+    }
 }
 
 // IPAddressControl.cpp
@@ -2987,6 +3296,11 @@ void sw::Layer::EnableLayout()
 {
     this->_layoutDisabled = false;
     this->UpdateLayout();
+}
+
+bool sw::Layer::IsLayoutDisabled()
+{
+    return this->_layoutDisabled;
 }
 
 void sw::Layer::GetHorizontalScrollRange(double &refMin, double &refMax)
@@ -7824,9 +8138,37 @@ void sw::Window::OnInactived()
 
 void sw::Window::OnDpiChanged(int dpiX, int dpiY)
 {
+    bool layoutDisabled = this->IsLayoutDisabled();
+
     Dip::Update(dpiX, dpiY);
-    this->UpdateLayout();
+    this->DisableLayout();
+
+    {
+        // Windows在DIP改变时会自动调整窗口大小，此时会先触发WM_WINDOWPOSCHANGED，再触发WM_DPICHANGED
+        // 因此在先触发的WM_WINDOWPOSCHANGED消息中，（Dip类中）DPI信息未更新，从而导致窗口的Rect数据错误
+        // 此处在更新DPI信息后手动发送一个WM_WINDOWPOSCHANGED以修正窗口的Rect数据
+
+        HWND hwnd = this->Handle;
+
+        RECT rect;
+        GetWindowRect(hwnd, &rect);
+
+        WINDOWPOS pos{};
+        pos.hwnd  = hwnd;
+        pos.x     = rect.left;
+        pos.y     = rect.top;
+        pos.cx    = rect.right - rect.left;
+        pos.cy    = rect.bottom - rect.top;
+        pos.flags = SWP_NOACTIVATE | SWP_NOZORDER;
+
+        this->SendMessageW(WM_WINDOWPOSCHANGED, 0, reinterpret_cast<LPARAM>(&pos));
+    }
+
     _UpdateFontForAllChild(*this);
+
+    if (!layoutDisabled) {
+        this->EnableLayout();
+    }
 }
 
 void sw::Window::Show()
@@ -8316,10 +8658,14 @@ LRESULT sw::WndBase::WndProc(const ProcMsg &refMsg)
             static double &scaleY = const_cast<double &>(Dip::ScaleY.Get());
 
             PWINDOWPOS pWndPos = reinterpret_cast<PWINDOWPOS>(refMsg.lParam);
-            this->_rect.left   = scaleX * pWndPos->x;
-            this->_rect.top    = scaleY * pWndPos->y;
-            this->_rect.width  = scaleX * pWndPos->cx;
-            this->_rect.height = scaleY * pWndPos->cy;
+            if ((pWndPos->flags & SWP_NOMOVE) == 0) {
+                this->_rect.left = scaleX * pWndPos->x;
+                this->_rect.top  = scaleY * pWndPos->y;
+            }
+            if ((pWndPos->flags & SWP_NOSIZE) == 0) {
+                this->_rect.width  = scaleX * pWndPos->cx;
+                this->_rect.height = scaleY * pWndPos->cy;
+            }
             return this->DefaultWndProc(refMsg);
         }
 
