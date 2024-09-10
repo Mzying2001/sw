9d5f69c903ea8a1ee71343b5dbc1c60ae980c716
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2024-09-10 23:16:50 +0800

    添加MsgBoxResultHelper作为消息框函数的返回值

--- sw/src/MsgBox.cpp
+++ sw/src/MsgBox.cpp
@@ -1,88 +1,86 @@
 #include "MsgBox.h"
 
-sw::MsgBox::MsgBox(MsgBoxResult result)
+sw::MsgBoxResultHelper::MsgBoxResultHelper(MsgBoxResult result)
     : result(result)
 {
 }
 
-sw::MsgBox sw::MsgBox::Show(const WndBase *owner, const std::wstring &text, const std::wstring &caption, MsgBoxButton button)
+sw::MsgBoxResultHelper::operator sw::MsgBoxResult() const
 {
-    HWND hwnd = owner == nullptr ? reinterpret_cast<HWND>(NULL) : owner->Handle;
+    return this->result;
+}
+
+sw::MsgBoxResultHelper &sw::MsgBoxResultHelper::OnOk(const MsgBoxCallback &callback)
+{
+    return this->On<MsgBoxResult::Ok>(callback);
+}
+
+sw::MsgBoxResultHelper &sw::MsgBoxResultHelper::OnYes(const MsgBoxCallback &callback)
+{
+    return this->On<MsgBoxResult::Yes>(callback);
+}
+
+sw::MsgBoxResultHelper &sw::MsgBoxResultHelper::OnNo(const MsgBoxCallback &callback)
+{
+    return this->On<MsgBoxResult::No>(callback);
+}
+
+sw::MsgBoxResultHelper &sw::MsgBoxResultHelper::OnCancel(const MsgBoxCallback &callback)
+{
+    return this->On<MsgBoxResult::Cancel>(callback);
+}
+
+sw::MsgBoxResultHelper sw::MsgBox::Show(const WndBase *owner, const std::wstring &text, const std::wstring &caption, MsgBoxButton button)
+{
+    HWND hwnd = (owner == nullptr) ? reinterpret_cast<HWND>(NULL) : owner->Handle;
     return (MsgBoxResult)MessageBoxW(hwnd, text.c_str(), caption.c_str(), (UINT)button);
 }
 
-sw::MsgBox sw::MsgBox::Show(const WndBase &owner, const std::wstring &text, const std::wstring &caption, MsgBoxButton button)
+sw::MsgBoxResultHelper sw::MsgBox::Show(const WndBase &owner, const std::wstring &text, const std::wstring &caption, MsgBoxButton button)
 {
     return MsgBox::Show(&owner, text, caption, button);
 }
 
-sw::MsgBox sw::MsgBox::ShowInfo(const WndBase *owner, const std::wstring &text, const std::wstring &caption, MsgBoxButton button)
+sw::MsgBoxResultHelper sw::MsgBox::ShowInfo(const WndBase *owner, const std::wstring &text, const std::wstring &caption, MsgBoxButton button)
 {
-    HWND hwnd = owner == nullptr ? reinterpret_cast<HWND>(NULL) : owner->Handle;
+    HWND hwnd = (owner == nullptr) ? reinterpret_cast<HWND>(NULL) : owner->Handle;
     return (MsgBoxResult)MessageBoxW(hwnd, text.c_str(), caption.c_str(), (UINT)button | MB_ICONINFORMATION);
 }
 
-sw::MsgBox sw::MsgBox::ShowInfo(const WndBase &owner, const std::wstring &text, const std::wstring &caption, MsgBoxButton button)
+sw::MsgBoxResultHelper sw::MsgBox::ShowInfo(const WndBase &owner, const std::wstring &text, const std::wstring &caption, MsgBoxButton button)
 {
     return MsgBox::ShowInfo(&owner, text, caption, button);
 }
 
-sw::MsgBox sw::MsgBox::ShowError(const WndBase *owner, const std::wstring &text, const std::wstring &caption, MsgBoxButton button)
+sw::MsgBoxResultHelper sw::MsgBox::ShowError(const WndBase *owner, const std::wstring &text, const std::wstring &caption, MsgBoxButton button)
 {
-    HWND hwnd = owner == nullptr ? reinterpret_cast<HWND>(NULL) : owner->Handle;
+    HWND hwnd = (owner == nullptr) ? reinterpret_cast<HWND>(NULL) : owner->Handle;
     return (MsgBoxResult)MessageBoxW(hwnd, text.c_str(), caption.c_str(), (UINT)button | MB_ICONERROR);
 }
 
-sw::MsgBox sw::MsgBox::ShowError(const WndBase &owner, const std::wstring &text, const std::wstring &caption, MsgBoxButton button)
+sw::MsgBoxResultHelper sw::MsgBox::ShowError(const WndBase &owner, const std::wstring &text, const std::wstring &caption, MsgBoxButton button)
 {
     return MsgBox::ShowError(&owner, text, caption, button);
 }
 
-sw::MsgBox sw::MsgBox::ShowWarning(const WndBase *owner, const std::wstring &text, const std::wstring &caption, MsgBoxButton button)
+sw::MsgBoxResultHelper sw::MsgBox::ShowWarning(const WndBase *owner, const std::wstring &text, const std::wstring &caption, MsgBoxButton button)
 {
-    HWND hwnd = owner == nullptr ? reinterpret_cast<HWND>(NULL) : owner->Handle;
+    HWND hwnd = (owner == nullptr) ? reinterpret_cast<HWND>(NULL) : owner->Handle;
     return (MsgBoxResult)MessageBoxW(hwnd, text.c_str(), caption.c_str(), (UINT)button | MB_ICONWARNING);
 }
 
-sw::MsgBox sw::MsgBox::ShowWarning(const WndBase &owner, const std::wstring &text, const std::wstring &caption, MsgBoxButton button)
+sw::MsgBoxResultHelper sw::MsgBox::ShowWarning(const WndBase &owner, const std::wstring &text, const std::wstring &caption, MsgBoxButton button)
 {
     return MsgBox::ShowWarning(&owner, text, caption, button);
 }
 
-sw::MsgBox sw::MsgBox::ShowQuestion(const WndBase *owner, const std::wstring &text, const std::wstring &caption, MsgBoxButton button)
+sw::MsgBoxResultHelper sw::MsgBox::ShowQuestion(const WndBase *owner, const std::wstring &text, const std::wstring &caption, MsgBoxButton button)
 {
-    HWND hwnd = owner == nullptr ? reinterpret_cast<HWND>(NULL) : owner->Handle;
+    HWND hwnd = (owner == nullptr) ? reinterpret_cast<HWND>(NULL) : owner->Handle;
     return (MsgBoxResult)MessageBoxW(hwnd, text.c_str(), caption.c_str(), (UINT)button | MB_ICONQUESTION);
 }
 
-sw::MsgBox sw::MsgBox::ShowQuestion(const WndBase &owner, const std::wstring &text, const std::wstring &caption, MsgBoxButton button)
+sw::MsgBoxResultHelper sw::MsgBox::ShowQuestion(const WndBase &owner, const std::wstring &text, const std::wstring &caption, MsgBoxButton button)
 {
     return MsgBox::ShowQuestion(&owner, text, caption, button);
-}
-
-const sw::MsgBox &sw::MsgBox::_On(MsgBoxResult result, const MsgBoxCallback &callback) const
-{
-    if (this->result == result && callback)
-        callback();
-    return *this;
-}
-
-const sw::MsgBox &sw::MsgBox::OnOk(const MsgBoxCallback &callback) const
-{
-    return this->_On(MsgBoxResult::Ok, callback);
-}
-
-const sw::MsgBox &sw::MsgBox::OnYes(const MsgBoxCallback &callback) const
-{
-    return this->_On(MsgBoxResult::Yes, callback);
-}
-
-const sw::MsgBox &sw::MsgBox::OnNo(const MsgBoxCallback &callback) const
-{
-    return this->_On(MsgBoxResult::No, callback);
-}
-
-const sw::MsgBox &sw::MsgBox::OnCancel(const MsgBoxCallback &callback) const
-{
-    return this->_On(MsgBoxResult::Cancel, callback);
 }
