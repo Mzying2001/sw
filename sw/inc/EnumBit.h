9eb968226c60d83fbdf54df45a82116fa70bd44f
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2024-09-15 23:01:48 +0800

    添加EnumBit.h

---
+++ sw/inc/EnumBit.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <type_traits>
+
+namespace sw
+{
+    /**
+     * @brief 用于标记枚举是否支持位运算
+     */
+    template <typename T>
+    struct _EnumSupportBitOperations : std::false_type {
+    };
+
+    /**
+     * @brief 为标记_EnumSupportBitOperations的枚举类型提供按位或运算
+     */
+    template <typename T>
+    inline constexpr typename std::enable_if<std::is_enum<T>::value && _EnumSupportBitOperations<T>::value, T>::type
+    operator|(T a, T b)
+    {
+        using TUnderlying = typename std::underlying_type<T>::type;
+        return static_cast<T>(static_cast<TUnderlying>(a) | static_cast<TUnderlying>(b));
+    }
+
+    /**
+     * @brief 为标记_EnumSupportBitOperations的枚举类型提供按位与运算
+     */
+    template <typename T>
+    inline constexpr typename std::enable_if<std::is_enum<T>::value && _EnumSupportBitOperations<T>::value, T>::type
+    operator&(T a, T b)
+    {
+        using TUnderlying = typename std::underlying_type<T>::type;
+        return static_cast<T>(static_cast<TUnderlying>(a) & static_cast<TUnderlying>(b));
+    }
+
+    /**
+     * @brief 为标记_EnumSupportBitOperations的枚举类型提供按位异或运算
+     */
+    template <typename T>
+    inline constexpr typename std::enable_if<std::is_enum<T>::value && _EnumSupportBitOperations<T>::value, T>::type
+    operator^(T a, T b)
+    {
+        using TUnderlying = typename std::underlying_type<T>::type;
+        return static_cast<T>(static_cast<TUnderlying>(a) ^ static_cast<TUnderlying>(b));
+    }
+
+    /**
+     * @brief 为标记_EnumSupportBitOperations的枚举类型提供按位取反运算
+     */
+    template <typename T>
+    inline constexpr typename std::enable_if<std::is_enum<T>::value && _EnumSupportBitOperations<T>::value, T>::type
+    operator~(T a)
+    {
+        using TUnderlying = typename std::underlying_type<T>::type;
+        return static_cast<T>(~static_cast<TUnderlying>(a));
+    }
+}
