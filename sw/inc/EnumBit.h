1cc3ef22664af7261374e67f1ba6ce9e6e40e5e5
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2024-09-17 16:46:19 +0800

    添加位赋值运算重载

--- sw/inc/EnumBit.h
+++ sw/inc/EnumBit.h
@@ -23,6 +23,16 @@ namespace sw
     }
 
     /**
+     * @brief 为标记_EnumSupportBitOperations的枚举类型提供按位或赋值运算
+     */
+    template <typename T>
+    inline constexpr typename std::enable_if<std::is_enum<T>::value && _EnumSupportBitOperations<T>::value, T &>::type
+    operator|=(T &a, T b)
+    {
+        return a = a | b;
+    }
+
+    /**
      * @brief 为标记_EnumSupportBitOperations的枚举类型提供按位与运算
      */
     template <typename T>
@@ -34,6 +44,16 @@ namespace sw
     }
 
     /**
+     * @brief 为标记_EnumSupportBitOperations的枚举类型提供按位与赋值运算
+     */
+    template <typename T>
+    inline constexpr typename std::enable_if<std::is_enum<T>::value && _EnumSupportBitOperations<T>::value, T &>::type
+    operator&=(T &a, T b)
+    {
+        return a = a & b;
+    }
+
+    /**
      * @brief 为标记_EnumSupportBitOperations的枚举类型提供按位异或运算
      */
     template <typename T>
@@ -42,6 +62,16 @@ namespace sw
     {
         using TUnderlying = typename std::underlying_type<T>::type;
         return static_cast<T>(static_cast<TUnderlying>(a) ^ static_cast<TUnderlying>(b));
+    }
+
+    /**
+     * @brief 为标记_EnumSupportBitOperations的枚举类型提供按位异或赋值运算
+     */
+    template <typename T>
+    inline constexpr typename std::enable_if<std::is_enum<T>::value && _EnumSupportBitOperations<T>::value, T &>::type
+    operator^=(T &a, T b)
+    {
+        return a = a ^ b;
     }
 
     /**
