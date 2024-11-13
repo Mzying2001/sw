30e41b684fbc5bb8477aaf6645600b44ef684522
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2024-11-13 19:29:06 +0800

    调整ToWideStr和ToMultiByteStr

--- sw/src/Utils.cpp
+++ sw/src/Utils.cpp
@@ -6,8 +6,8 @@ std::wstring sw::Utils::ToWideStr(const std::string &str, bool utf8)
 {
     int code = utf8 ? CP_UTF8 : CP_ACP;
     int size = MultiByteToWideChar(code, 0, str.c_str(), -1, nullptr, 0);
-    std::wstring wstr(size - 1, L'\0');
-    MultiByteToWideChar(code, 0, str.c_str(), -1, &wstr[0], size);
+    std::wstring wstr(size, L'\0');
+    wstr.resize(MultiByteToWideChar(code, 0, str.c_str(), -1, &wstr[0], size));
     return wstr;
 }
 
@@ -15,8 +15,8 @@ std::string sw::Utils::ToMultiByteStr(const std::wstring &wstr, bool utf8)
 {
     int code = utf8 ? CP_UTF8 : CP_ACP;
     int size = WideCharToMultiByte(code, 0, wstr.c_str(), -1, nullptr, 0, nullptr, nullptr);
-    std::string str(size - 1, '\0');
-    WideCharToMultiByte(code, 0, wstr.c_str(), -1, &str[0], size, nullptr, nullptr);
+    std::string str(size, '\0');
+    str.resize(WideCharToMultiByte(code, 0, wstr.c_str(), -1, &str[0], size, nullptr, nullptr));
     return str;
 }
 
