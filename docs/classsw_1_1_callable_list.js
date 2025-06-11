23a81714e96c7f89350ca2424a32b98c822b5049
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2025-06-11 20:20:32 +0800

    更新单文件和文档

--- docs/classsw_1_1_callable_list.js
+++ docs/classsw_1_1_callable_list.js
@@ -1,8 +1,8 @@
 var classsw_1_1_callable_list =
 [
     [ "TCallable", "classsw_1_1_callable_list.html#a2a852c7aa6b4637bbad845d97c746ad4", null ],
-    [ "TList", "classsw_1_1_callable_list.html#a26d57b2b0a4a0f025c11c6db18c2c691", null ],
-    [ "TPtr", "classsw_1_1_callable_list.html#a4696d7306e80c69cef39fa14195e0a2f", null ],
+    [ "TSharedList", "classsw_1_1_callable_list.html#ac83c29c7021a79ad217c8bf2b4faae57", null ],
+    [ "TSinglePtr", "classsw_1_1_callable_list.html#a65046366d6567d3dcb6446db1be4f580", null ],
     [ "CallableList", "classsw_1_1_callable_list.html#a432ed808aedc7d2a1b46436ee1d11281", null ],
     [ "CallableList", "classsw_1_1_callable_list.html#a3766869619981026aef4530d1c252085", null ],
     [ "CallableList", "classsw_1_1_callable_list.html#a337d73adf784038508899e699a0c865b", null ],
--- docs/search/typedefs_0.js
+++ docs/search/typedefs_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['tcallable_0',['TCallable',['../classsw_1_1_callable_list.html#a2a852c7aa6b4637bbad845d97c746ad4',1,'sw::CallableList']]],
-  ['tlist_1',['TList',['../classsw_1_1_callable_list.html#a26d57b2b0a4a0f025c11c6db18c2c691',1,'sw::CallableList']]],
-  ['tptr_2',['TPtr',['../classsw_1_1_callable_list.html#a4696d7306e80c69cef39fa14195e0a2f',1,'sw::CallableList']]]
+  ['tsharedlist_1',['TSharedList',['../classsw_1_1_callable_list.html#ac83c29c7021a79ad217c8bf2b4faae57',1,'sw::CallableList']]],
+  ['tsingleptr_2',['TSinglePtr',['../classsw_1_1_callable_list.html#a65046366d6567d3dcb6446db1be4f580',1,'sw::CallableList']]]
 ];
