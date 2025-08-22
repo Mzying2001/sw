dfbe1eca9148d84bdcf8c9072f8ee324a2528ad0
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2025-08-22 23:28:44 +0800

    添加`CommandLink`控件

---
+++ sw/inc/CommandLink.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "ButtonBase.h"
+
+namespace sw
+{
+    /**
+     * @brief 按钮
+     */
+    class CommandLink : public ButtonBase
+    {
+    public:
+        /**
+         * @brief 描述文字
+         */
+        const Property<std::wstring> NoteText;
+
+        /**
+         * @brief 初始化按钮
+         */
+        CommandLink();
+
+    protected:
+        /**
+         * @brief         更新按钮样式
+         * @param focused 是否处于焦点状态
+         */
+        virtual void UpdateButtonStyle(bool focused);
+
+        /**
+         * @brief           接收到WM_SETFOCUS时调用该函数
+         * @param hPreFocus 丢失焦点的hwnd，可能为NULL
+         * @return          若已处理该消息则返回true，否则返回false以调用DefaultWndProc
+         */
+        virtual bool OnSetFocus(HWND hPreFocus) override;
+
+        /**
+         * @brief            接收到WM_KILLFOCUS时调用该函数
+         * @param hNextFocus 接收到焦点的hwnd，可能为NULL
+         * @return           若已处理该消息则返回true，否则返回false以调用DefaultWndProc
+         */
+        virtual bool OnKillFocus(HWND hNextFocus) override;
+
+        /**
+         * @brief       接收到WM_KEYDOWN时调用该函数
+         * @param key   虚拟按键
+         * @param flags 附加信息
+         * @return      若已处理该消息则返回true，否则返回false以调用DefaultWndProc
+         */
+        virtual bool OnKeyDown(VirtualKey key, KeyFlags flags) override;
+    };
+}
---
+++ sw/src/CommandLink.cpp
@@ -0,0 +1,57 @@
+#include "CommandLink.h"
+
+sw::CommandLink::CommandLink()
+    : NoteText(
+          // get
+          [this]() -> std::wstring {
+              int len = (int)SendMessageW(BCM_GETNOTELENGTH, 0, 0);
+              if (len <= 0)
+                  return std::wstring{};
+              else {
+                  std::wstring result;
+                  result.resize(len + 1);
+                  SendMessageW(BCM_GETNOTE, len + 1, reinterpret_cast<LPARAM>(&result[0]));
+                  result.resize(len);
+                  return result;
+              }
+          },
+          // set
+          [this](const std::wstring &value) {
+              SendMessageW(BCM_SETNOTE, 0, reinterpret_cast<LPARAM>(value.c_str()));
+              if (AutoSize) InvalidateMeasure();
+          })
+{
+    InitButtonBase(L"CommandLink", WS_CHILD | WS_VISIBLE | WS_CLIPSIBLINGS | BS_NOTIFY | BS_COMMANDLINK, 0);
+    Rect     = sw::Rect{0, 0, 180, 60};
+    NoteText = L"Description";
+}
+
+void sw::CommandLink::UpdateButtonStyle(bool focused)
+{
+    auto style = GetStyle() & ~(BS_COMMANDLINK | BS_DEFCOMMANDLINK);
+    SetStyle(focused ? (style | BS_DEFCOMMANDLINK) : (style | BS_COMMANDLINK));
+}
+
+bool sw::CommandLink::OnSetFocus(HWND hPreFocus)
+{
+    UpdateButtonStyle(true);
+    Redraw();
+    return ButtonBase::OnSetFocus(hPreFocus);
+}
+
+bool sw::CommandLink::OnKillFocus(HWND hNextFocus)
+{
+    UpdateButtonStyle(false);
+    Redraw();
+    return ButtonBase::OnKillFocus(hNextFocus);
+}
+
+bool sw::CommandLink::OnKeyDown(VirtualKey key, KeyFlags flags)
+{
+    bool result = UIElement::OnKeyDown(key, flags);
+
+    if (!result && key == VirtualKey::Enter) {
+        OnClicked();
+    }
+    return result;
+}
