d5e0a26a1817b81febffb26a61456789d22e6493
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2025-09-02 22:36:42 +0800

    添加`DockSplitter`示例
    Introduces a DockSplitter for adjustable group box width and updates layout properties for groupBox and stackPanel. Reduces button count to 50 and refines button initialization for improved UI consistency.

--- examples/07_scrollbar_and_float/src/MyWindow.hpp
+++ examples/07_scrollbar_and_float/src/MyWindow.hpp
@@ -11,11 +11,14 @@ class MyWindow : public sw::Window
     // 组合框，该元素悬浮于界面左边
     sw::GroupBox groupBox;
 
+    // 分隔条，用户可拖动该分隔条调整组合框宽度
+    sw::DockSplitter splitter;
+
     // 堆叠面板，用于摆放按钮
     sw::StackPanel stackPanel;
 
     // 按钮个数
-    static constexpr int BTN_COUNT = 100;
+    static constexpr int BTN_COUNT = 50;
 
     // 按钮数组
     sw::Button btnarr[BTN_COUNT];
@@ -31,23 +34,30 @@ class MyWindow : public sw::Window
         // 将组合框设置为悬浮并添加到窗口
         // 将Float属性设置为true即可将元素设为悬浮，悬浮的元素位置不会受到滚动条影响
         groupBox.Float = true;
-        groupBox.Width = 200;
-        groupBox.Margin = sw::Thickness(10, 10, 0, 10);
+        groupBox.Margin = {10, 10, 0, 10};
+        groupBox.MinWidth = 100;
+        groupBox.MaxWidth = 500;
         groupBox.VerticalAlignment = sw::VerticalAlignment::Stretch;
         AddChild(groupBox, sw::DockLayoutTag::Left);
+
+        // 将分隔条添加到窗口
+        splitter.Float = true;
+        splitter.DrawSplitterLine = false;
+        splitter.Orientation = sw::Orientation::Vertical;
+        AddChild(splitter, sw::DockLayoutTag::Left);
 
         // 添加按钮到界面
         for (int i = 0; i < BTN_COUNT; ++i)
         {
-            sw::Button &btn = btnarr[i];
-            btn.Text = sw::Utils::BuildStr(L"Button", i + 1);
-            btn.Margin = sw::Thickness(0, i ? 10 : 0, 0, 0);
+            auto &btn = btnarr[i];
+            btn.Text = L"Button" + std::to_wstring(i + 1);
+            btn.Margin = {0, 10, 0, 0};
             btn.HorizontalAlignment = sw::HorizontalAlignment::Stretch;
             stackPanel.AddChild(btn);
         }
 
         // 将堆叠面板的垂直对齐方式设为Top，使之再垂直方向尺寸不会被限制
-        stackPanel.Margin = 10;
+        stackPanel.Margin = {0, 0, 10, 10};
         stackPanel.VerticalAlignment = sw::VerticalAlignment::Top;
         AddChild(stackPanel);
 
