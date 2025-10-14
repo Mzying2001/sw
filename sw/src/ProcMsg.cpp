72d47aa002e14b0dd52fe86fc405a061f430b9d7
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2025-10-14 22:31:44 +0800

    调整`ProcMsg`默认构造函数
    Replaces the custom default constructor of ProcMsg with the compiler-generated default constructor. Removes redundant constructor definition from ProcMsg.cpp for cleaner and more maintainable code.

--- sw/src/ProcMsg.cpp
+++ sw/src/ProcMsg.cpp
@@ -1,10 +1,5 @@
 #include "ProcMsg.h"
 
-sw::ProcMsg::ProcMsg()
-    : ProcMsg(NULL, 0, 0, 0)
-{
-}
-
 sw::ProcMsg::ProcMsg(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     : hwnd(hwnd), uMsg(uMsg), wParam(wParam), lParam(lParam)
 {
