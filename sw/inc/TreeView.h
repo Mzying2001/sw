1d564bd37036550c64cc0e12607bcd7f66142cae
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2025-10-17 23:28:29 +0800

    实现`TreeView`控件
    Added TreeViewNode class with methods for manipulating tree nodes, including selection, expansion, deletion, and text management. Refactored TreeView to inherit from ItemsControl<TreeViewNode> and implemented core tree operations, item access, and property accessors for root and item count.

--- sw/inc/TreeView.h
+++ sw/inc/TreeView.h
@@ -1,12 +1,288 @@
-#include "Control.h"
+#include "ItemsControl.h"
+#include <CommCtrl.h>
 
 namespace sw
 {
     /**
+     * @brief 树视图项
+     */
+    class TreeViewNode : public IToString<TreeViewNode>,
+                         public IEqualityComparable<TreeViewNode>
+    {
+    private:
+        /**
+         * @brief 树视图控件的窗口句柄
+         */
+        HWND _hwnd;
+
+        /**
+         * @brief 树视图项的句柄
+         */
+        HTREEITEM _hitem;
+
+    public:
+        /**
+         * @brief 默认构造函数
+         */
+        TreeViewNode() = default;
+
+        /**
+         * @brief 创建TreeViewItem
+         */
+        TreeViewNode(HWND hwnd, HTREEITEM hitem);
+
+        /**
+         * @brief 获取所属树视图控件的窗口句柄
+         */
+        HWND GetOwnerHandle() const;
+
+        /**
+         * @brief 获取当前项的句柄
+         */
+        HTREEITEM GetHandle() const;
+
+        /**
+         * @brief 获取当前项的文本
+         */
+        std::wstring ToString() const;
+
+        /**
+         * @brief 判断当前项与另一个项是否相等
+         */
+        bool Equals(const TreeViewNode &other) const;
+
+        /**
+         * @brief 判断当前项是否为空
+         */
+        bool IsNull() const;
+
+        /**
+         * @brief 判断当前项是否有效
+         */
+        operator bool() const;
+
+        /**
+         * @brief 获取当前项的文本
+         */
+        std::wstring GetText() const;
+
+        /**
+         * @brief  设置当前项的文本
+         * @return 操作是否成功
+         */
+        bool SetText(const std::wstring &text) const;
+
+        /**
+         * @brief  获取父节点
+         * @return 父节点，若无父节点则返回空节点
+         */
+        TreeViewNode GetParent() const;
+
+        /**
+         * @brief  获取下一个节点
+         * @return 下一个节点，若无下一个节点则返回空节点
+         */
+        TreeViewNode GetNextNode() const;
+
+        /**
+         * @brief  获取上一个节点
+         * @return 上一个节点，若无上一个节点则返回空节点
+         */
+        TreeViewNode GetPreviousNode() const;
+
+        /**
+         * @brief  获取第一个子节点
+         * @return 第一个子节点，若无子节点则返回空节点
+         */
+        TreeViewNode GetFirstChildNode() const;
+
+        /**
+         * @brief  在当前节点后插入新节点
+         * @return 新插入的节点
+         */
+        TreeViewNode InsertAfter(const std::wstring &text) const;
+
+        /**
+         * @brief  添加子节点到当前节点下
+         * @return 新插入的节点
+         */
+        TreeViewNode AddChild(const std::wstring &text) const;
+
+        /**
+         * @brief  判断当前节点是否被选中
+         * @return 若节点被选中则返回true，否则返回false
+         */
+        bool IsSelected() const;
+
+        /**
+         * @brief  选中当前节点
+         * @return 操作是否成功
+         */
+        bool Select() const;
+
+        /**
+         * @brief  删除当前节点
+         * @return 操作是否成功
+         */
+        bool Delete() const;
+
+        /**
+         * @brief  判断当前节点是否展开
+         * @return 若节点已展开则返回true，否则返回false
+         */
+        bool IsExpanded() const;
+
+        /**
+         * @brief  设置当前节点展开或折叠
+         * @return 操作是否成功
+         */
+        bool SetExpanded(bool expand) const;
+
+        /**
+         * @brief  展开当前节点
+         * @return 操作是否成功
+         */
+        bool Expand() const;
+
+        /**
+         * @brief  折叠当前节点
+         * @return 操作是否成功
+         */
+        bool Collapse() const;
+    };
+
+    /**
      * @brief 树视图控件
      */
-    class TreeView : public Control
+    class TreeView : public ItemsControl<TreeViewNode>
     {
-        // TODO
+    public:
+        /**
+         * @brief 根节点
+         */
+        ReadOnlyProperty<TreeViewNode> Root;
+
+        /**
+         * @brief 所有节点数
+         */
+        ReadOnlyProperty<int> AllItemsCount;
+
+    public:
+        /**
+         * @brief 初始化TreeView
+         */
+        TreeView();
+
+    protected:
+        /**
+         * @brief  获取子项数
+         */
+        virtual int GetItemsCount() override;
+
+        /**
+         * @brief 选中项的索引，当无选中项时为-1
+         */
+        virtual int GetSelectedIndex() override;
+
+        /**
+         * @brief 设置选中项索引
+         */
+        virtual void SetSelectedIndex(int index) override;
+
+        /**
+         * @brief 获取选中项
+         */
+        virtual TreeViewNode GetSelectedItem() override;
+
+        /**
+         * @brief     绘制虚线框时调用该函数
+         * @param hdc 绘制设备句柄
+         */
+        virtual void OnDrawFocusRect(HDC hdc) override;
+
+        /**
+         * @brief        设置背景颜色
+         * @param color  要设置的颜色
+         * @param redraw 是否重绘
+         */
+        virtual void SetBackColor(Color color, bool redraw) override;
+
+        /**
+         * @brief        设置文本颜色
+         * @param color  要设置的颜色
+         * @param redraw 是否重绘
+         */
+        virtual void SetTextColor(Color color, bool redraw) override;
+
+    public:
+        /**
+         * @brief 清空所有子项
+         */
+        virtual void Clear() override;
+
+        /**
+         * @brief       获取指定索引处子项的值
+         * @param index 子项的索引
+         */
+        virtual TreeViewNode GetItemAt(int index) override;
+
+        /**
+         * @brief      添加新的子项
+         * @param item 要添加的子项
+         * @return     是否添加成功
+         * @note       TreeView不支持该操作，该函数始终返回false
+         */
+        virtual bool AddItem(const TreeViewNode &item) override;
+
+        /**
+         * @brief       添加子项到指定索引
+         * @param index 要插入的位置
+         * @param item  要添加的子项
+         * @return      是否添加成功
+         * @note        TreeView不支持该操作，该函数始终返回false
+         */
+        virtual bool InsertItem(int index, const TreeViewNode &item) override;
+
+        /**
+         * @brief          更新指定位置的子项
+         * @param index    要更新子项的位置
+         * @param newValue 子项的新值
+         * @return         操作是否成功
+         * @note           TreeView不支持该操作，该函数始终返回false
+         */
+        virtual bool UpdateItem(int index, const TreeViewNode &newValue) override;
+
+        /**
+         * @brief       移除指定索引处的子项
+         * @param index 要移除子项的索引
+         * @return      操作是否成功
+         */
+        virtual bool RemoveItemAt(int index) override;
+
+        /**
+         * @brief      添加新节点到根节点
+         * @param text 节点文本
+         * @return     新插入的节点
+         */
+        TreeViewNode AddItem(const std::wstring &text);
+
+        /**
+         * @brief       在指定索引处插入新节点
+         * @param index 节点索引
+         * @param text  节点文本
+         * @return      新插入的节点
+         */
+        TreeViewNode InsertItem(int index, const std::wstring &text);
+
+    private:
+        /**
+         * @brief 获取根节点
+         */
+        TreeViewNode _GetRoot();
+
+        /**
+         * @brief 插入新节点
+         */
+        TreeViewNode _InsertItem(HTREEITEM hParent, HTREEITEM hInsertAfter, const std::wstring &text);
     };
 }
--- sw/src/TreeView.cpp
+++ sw/src/TreeView.cpp
@@ -1,1 +1,314 @@
 #include "TreeView.h"
+
+sw::TreeViewNode::TreeViewNode(HWND hwnd, HTREEITEM hitem)
+    : _hwnd(hwnd), _hitem(hitem)
+{
+}
+
+HWND sw::TreeViewNode::GetOwnerHandle() const
+{
+    return _hwnd;
+}
+
+HTREEITEM sw::TreeViewNode::GetHandle() const
+{
+    return _hitem;
+}
+
+std::wstring sw::TreeViewNode::ToString() const
+{
+    return GetText();
+}
+
+bool sw::TreeViewNode::Equals(const TreeViewNode &other) const
+{
+    return _hwnd == other._hwnd && _hitem == other._hitem;
+}
+
+bool sw::TreeViewNode::IsNull() const
+{
+    return _hwnd == NULL || _hitem == NULL;
+}
+
+sw::TreeViewNode::operator bool() const
+{
+    return !IsNull();
+}
+
+std::wstring sw::TreeViewNode::GetText() const
+{
+    std::wstring text;
+    text.resize(256);
+
+    TVITEMW tvi{};
+    tvi.mask  = TVIF_TEXT;
+    tvi.hItem = _hitem;
+
+    while (true) {
+        tvi.pszText    = &text[0];
+        tvi.cchTextMax = static_cast<int>(text.size());
+
+        if (::SendMessageW(_hwnd, TVM_GETITEMW, 0, reinterpret_cast<LPARAM>(&tvi)) == FALSE) {
+            text.clear();
+            break;
+        }
+
+        size_t len = wcslen(tvi.pszText);
+
+        if (len < text.size() - 1) {
+            text.resize(len);
+            break;
+        } else {
+            text.resize(text.size() * 2);
+        }
+    }
+    return text;
+}
+
+bool sw::TreeViewNode::SetText(const std::wstring &text) const
+{
+    TVITEMW tvi{};
+    tvi.mask    = TVIF_TEXT;
+    tvi.hItem   = _hitem;
+    tvi.pszText = const_cast<LPWSTR>(text.c_str());
+
+    return ::SendMessageW(_hwnd, TVM_SETITEMW, 0, reinterpret_cast<LPARAM>(&tvi)) != FALSE;
+}
+
+sw::TreeViewNode sw::TreeViewNode::GetParent() const
+{
+    return TreeViewNode{_hwnd, TreeView_GetParent(_hwnd, _hitem)};
+}
+
+sw::TreeViewNode sw::TreeViewNode::GetNextNode() const
+{
+    return TreeViewNode{_hwnd, TreeView_GetNextSibling(_hwnd, _hitem)};
+}
+
+sw::TreeViewNode sw::TreeViewNode::GetPreviousNode() const
+{
+    return TreeViewNode{_hwnd, TreeView_GetPrevSibling(_hwnd, _hitem)};
+}
+
+sw::TreeViewNode sw::TreeViewNode::GetFirstChildNode() const
+{
+    return TreeViewNode{_hwnd, TreeView_GetChild(_hwnd, _hitem)};
+}
+
+sw::TreeViewNode sw::TreeViewNode::InsertAfter(const std::wstring &text) const
+{
+    TVINSERTSTRUCTW tvis{};
+    tvis.hParent      = TreeView_GetParent(_hwnd, _hitem);
+    tvis.hInsertAfter = _hitem;
+    tvis.item.mask    = TVIF_TEXT;
+    tvis.item.pszText = const_cast<LPWSTR>(text.c_str());
+
+    auto hitem = (HTREEITEM)SendMessageW(_hwnd, TVM_INSERTITEMW, 0, reinterpret_cast<LPARAM>(&tvis));
+    return TreeViewNode{_hwnd, hitem};
+}
+
+sw::TreeViewNode sw::TreeViewNode::AddChild(const std::wstring &text) const
+{
+    TVINSERTSTRUCTW tvis{};
+    tvis.hParent      = _hitem;
+    tvis.hInsertAfter = TVI_LAST;
+    tvis.item.mask    = TVIF_TEXT;
+    tvis.item.pszText = const_cast<LPWSTR>(text.c_str());
+
+    auto hitem = (HTREEITEM)SendMessageW(_hwnd, TVM_INSERTITEMW, 0, reinterpret_cast<LPARAM>(&tvis));
+    return TreeViewNode{_hwnd, hitem};
+}
+
+bool sw::TreeViewNode::IsSelected() const
+{
+    return (TreeView_GetItemState(_hwnd, _hitem, TVIS_SELECTED) & TVIS_SELECTED) != 0;
+}
+
+bool sw::TreeViewNode::Select() const
+{
+    return TreeView_SelectItem(_hwnd, _hitem);
+}
+
+bool sw::TreeViewNode::Delete() const
+{
+    return TreeView_DeleteItem(_hwnd, _hitem);
+}
+
+bool sw::TreeViewNode::IsExpanded() const
+{
+    return (TreeView_GetItemState(_hwnd, _hitem, TVIS_EXPANDED) & TVIS_EXPANDED) != 0;
+}
+
+bool sw::TreeViewNode::SetExpanded(bool expand) const
+{
+    UINT action = expand ? TVE_EXPAND : TVE_COLLAPSE;
+    return TreeView_Expand(_hwnd, _hitem, action);
+}
+
+bool sw::TreeViewNode::Expand() const
+{
+    return SetExpanded(true);
+}
+
+bool sw::TreeViewNode::Collapse() const
+{
+    return SetExpanded(false);
+}
+
+sw::TreeView::TreeView()
+    : Root(
+          // get
+          [this]() -> TreeViewNode {
+              return _GetRoot();
+          }),
+
+      AllItemsCount(
+          // get
+          [this]() -> int {
+              HWND hwnd = Handle;
+              return TreeView_GetCount(hwnd);
+          })
+{
+    InitControl(WC_TREEVIEWW, NULL, WS_VISIBLE | WS_CHILD | WS_CLIPSIBLINGS | WS_BORDER | TVS_HASLINES | TVS_LINESATROOT | TVS_HASBUTTONS, 0);
+
+    Rect    = {0, 0, 200, 200};
+    TabStop = true;
+}
+
+int sw::TreeView::GetItemsCount()
+{
+    int cnt = 0;
+
+    TreeViewNode node = _GetRoot();
+    while (!node.IsNull()) {
+        cnt++;
+        node = node.GetNextNode();
+    }
+    return cnt;
+}
+
+int sw::TreeView::GetSelectedIndex()
+{
+    int index = 0;
+
+    TreeViewNode node = _GetRoot();
+    while (!node.IsNull()) {
+        if (node.IsSelected()) {
+            return index;
+        } else {
+            ++index;
+            node = node.GetNextNode();
+        }
+    }
+    return -1;
+}
+
+void sw::TreeView::SetSelectedIndex(int index)
+{
+    TreeViewNode node = GetItemAt(index);
+    if (!node.IsNull()) node.Select();
+}
+
+sw::TreeViewNode sw::TreeView::GetSelectedItem()
+{
+    HWND hwnd = Handle;
+    return TreeViewNode{hwnd, TreeView_GetSelection(hwnd)};
+}
+
+void sw::TreeView::OnDrawFocusRect(HDC hdc)
+{
+    // 不绘制虚线框
+}
+
+void sw::TreeView::SetBackColor(Color color, bool redraw)
+{
+    Control::SetBackColor(color, false);
+
+    HWND hwnd = Handle;
+    TreeView_SetBkColor(hwnd, static_cast<COLORREF>(color));
+}
+
+void sw::TreeView::SetTextColor(Color color, bool redraw)
+{
+    Control::SetTextColor(color, false);
+
+    HWND hwnd = Handle;
+    TreeView_SetTextColor(hwnd, static_cast<COLORREF>(color));
+}
+
+void sw::TreeView::Clear()
+{
+    HWND hwnd = Handle;
+    TreeView_DeleteAllItems(hwnd);
+}
+
+sw::TreeViewNode sw::TreeView::GetItemAt(int index)
+{
+    int curIndex = 0;
+
+    TreeViewNode node = _GetRoot();
+    while (!node.IsNull()) {
+        if (curIndex == index) {
+            return node;
+        } else {
+            ++curIndex;
+            node = node.GetNextNode();
+        }
+    }
+    return TreeViewNode{};
+}
+
+bool sw::TreeView::AddItem(const TreeViewNode &item)
+{
+    return false;
+}
+
+bool sw::TreeView::InsertItem(int index, const TreeViewNode &item)
+{
+    return false;
+}
+
+bool sw::TreeView::UpdateItem(int index, const TreeViewNode &newValue)
+{
+    return false;
+}
+
+bool sw::TreeView::RemoveItemAt(int index)
+{
+    TreeViewNode node = GetItemAt(index);
+    return node.IsNull() ? false : node.Delete();
+}
+
+sw::TreeViewNode sw::TreeView::AddItem(const std::wstring &text)
+{
+    return _InsertItem(TVI_ROOT, TVI_LAST, text);
+}
+
+sw::TreeViewNode sw::TreeView::InsertItem(int index, const std::wstring &text)
+{
+    if (index <= 0) {
+        return _InsertItem(TVI_ROOT, TVI_FIRST, text);
+    }
+    TreeViewNode prevNode = GetItemAt(index - 1);
+    return prevNode.IsNull() ? TreeViewNode{} : _InsertItem(TVI_ROOT, prevNode.GetHandle(), text);
+}
+
+sw::TreeViewNode sw::TreeView::_GetRoot()
+{
+    HWND hwnd = Handle;
+    return TreeViewNode{hwnd, TreeView_GetRoot(hwnd)};
+}
+
+sw::TreeViewNode sw::TreeView::_InsertItem(HTREEITEM hParent, HTREEITEM hInsertAfter, const std::wstring &text)
+{
+    HWND hwnd = Handle;
+
+    TVINSERTSTRUCTW tvis{};
+    tvis.hParent      = hParent;
+    tvis.hInsertAfter = hInsertAfter;
+    tvis.item.mask    = TVIF_TEXT;
+    tvis.item.pszText = const_cast<LPWSTR>(text.c_str());
+
+    auto hitem = (HTREEITEM)SendMessageW(TVM_INSERTITEMW, 0, reinterpret_cast<LPARAM>(&tvis));
+    return TreeViewNode{hwnd, hitem};
+}
