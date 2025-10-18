5aefbe5972e2ed10bbdc7f5d32e131a05aa8eef3
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2025-10-18 18:25:02 +0800

    添加判断属性可读写性的辅助模板
    Introduced helper templates to detect presence of GetterImpl and SetterImpl members, and added traits to determine if a type is a readable or writable property. These additions improve type introspection for property handling.

--- sw/inc/Property.h
+++ sw/inc/Property.h
@@ -77,6 +77,52 @@ namespace sw
     };
 
     /**
+     * @brief 判断类型是否有GetterImpl成员的辅助模板
+     */
+    template <typename, typename = void>
+    struct _HasGetterImpl : std::false_type {
+    };
+
+    /**
+     * @brief _HasGetterImpl模板特化
+     */
+    template <typename T>
+    struct _HasGetterImpl<
+        T, decltype(void(&T::GetterImpl))> : std::true_type {
+    };
+
+    /**
+     * @brief 判断类型是否有SetterImpl成员的辅助模板
+     */
+    template <typename, typename = void>
+    struct _HasSetterImpl : std::false_type {
+    };
+
+    /**
+     * @brief _HasSetterImpl模板特化
+     */
+    template <typename T>
+    struct _HasSetterImpl<
+        T, decltype(void(&T::SetterImpl))> : std::true_type {
+    };
+
+    /**
+     * @brief 判断类型是否为可读属性的辅助模板
+     */
+    template <typename T>
+    struct _IsReadableProperty
+        : std::integral_constant<bool, _IsProperty<T>::value && _HasGetterImpl<T>::value> {
+    };
+
+    /**
+     * @brief 判断类型是否为可写属性的辅助模板
+     */
+    template <typename T>
+    struct _IsWritableProperty
+        : std::integral_constant<bool, _IsProperty<T>::value && _HasSetterImpl<T>::value> {
+    };
+
+    /**
      * @brief 判断类型是否可以使用[]操作符的辅助模板
      */
     template <typename T, typename U, typename = void>
