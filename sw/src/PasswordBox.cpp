d6029a17ba8784a5bc49cf6c76b7f455f7fa51aa
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2025-12-09 21:18:07 +0800

    `PasswordBox`添加属性变更通知
    Only send EM_SETPASSWORDCHAR and raise the PasswordChar property changed event if the value actually changes. This prevents unnecessary notifications and updates.

--- sw/src/PasswordBox.cpp
+++ sw/src/PasswordBox.cpp
@@ -7,9 +7,12 @@ sw::PasswordBox::PasswordBox()
                   return (wchar_t)self->SendMessageW(EM_GETPASSWORDCHAR, 0, 0);
               })
               .Setter([](PasswordBox *self, wchar_t value) {
-                  self->SendMessageW(EM_SETPASSWORDCHAR, value, 0);
+                  if (self->PasswordChar != value) {
+                      self->SendMessageW(EM_SETPASSWORDCHAR, value, 0);
+                      self->RaisePropertyChanged(&PasswordBox::PasswordChar);
+                  }
               }))
 {
-    this->InitTextBoxBase(WS_CHILD | WS_VISIBLE | WS_CLIPSIBLINGS | ES_PASSWORD | ES_LEFT | ES_AUTOHSCROLL, WS_EX_CLIENTEDGE);
-    this->Rect = sw::Rect(0, 0, 100, 24);
+    InitTextBoxBase(WS_CHILD | WS_VISIBLE | WS_CLIPSIBLINGS | ES_PASSWORD | ES_LEFT | ES_AUTOHSCROLL, WS_EX_CLIENTEDGE);
+    Rect = sw::Rect(0, 0, 100, 24);
 }
