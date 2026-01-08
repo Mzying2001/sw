0ab1ab5a41e0c77f7cbc9eb3984b3b12e94f4f33
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2026-01-08 23:31:11 +0800

    添加`BoxedObject`类
    Introduced BoxedObject.h, a template class for boxing arbitrary types as DynamicObject, supporting both value and reference semantics. Updated SimpleWindow.h to include BoxedObject.h and registered the new header in the Visual Studio project files.

---
+++ sw/inc/BoxedObject.h
@@ -0,0 +1,263 @@
+#include "Reflection.h"
+
+namespace sw
+{
+    /**
+     * @brief 装箱对象模板类声明
+     */
+    template <typename T, typename = void>
+    class BoxedObject;
+
+    /**
+     * @brief 装箱对象，用于将任意类型封装为DynamicObject对象
+     */
+    template <typename T>
+    class BoxedObject<T, typename std::enable_if<!std::is_base_of<DynamicObject, T>::value>::type> final
+        : public DynamicObject
+    {
+    private:
+        /**
+         * @brief 指示当前对象是否为引用对象
+         */
+        bool _isRef;
+
+        /**
+         * @brief 内部数据联合体
+         */
+        union {
+            /**
+             * @brief 引用指针
+             */
+            T *refptr;
+
+            /**
+             * @brief 对象缓冲区
+             */
+            alignas(T) uint8_t objbuf[sizeof(T)];
+        } _data;
+
+    private:
+        /**
+         * @brief 内部结构体，用于标识是否为引用参数
+         */
+        struct _IsRefParam {
+            bool val;
+        };
+
+        /**
+         * @brief 私有构造函数，根据是否为引用参数初始化对象
+         * @param isRef 指示是否为引用参数
+         */
+        explicit BoxedObject(_IsRefParam isRef) noexcept
+            : _isRef(isRef.val), _data()
+        {
+        }
+
+        /**
+         * @brief 释放对象资源
+         */
+        void Release() noexcept(std::is_nothrow_destructible<T>::value)
+        {
+            if (!_isRef) {
+                Unbox().~T();
+            }
+        }
+
+    public:
+        /**
+         * @brief 构造值类型的装箱对象
+         * @param args 构造参数列表
+         */
+        template <typename... Args>
+        BoxedObject(Args &&...args)
+            : BoxedObject(_IsRefParam{false})
+        {
+            new (_data.objbuf) T(std::forward<Args>(args)...);
+        }
+
+        /**
+         * @brief 析构函数
+         */
+        virtual ~BoxedObject() noexcept(std::is_nothrow_destructible<T>::value)
+        {
+            Release();
+        }
+
+        /**
+         * @brief 创建引用类型的装箱对象
+         * @param ptr 指向外部对象的指针
+         * @return 引用类型的装箱对象
+         */
+        static BoxedObject<T> MakeRef(T *ptr) noexcept
+        {
+            BoxedObject<T> result{_IsRefParam{true}};
+            result._data.refptr = ptr;
+            return result;
+        }
+
+        /**
+         * @brief 创建引用类型的装箱对象
+         * @tparam U 目标类型
+         * @param other 另一个装箱对象
+         * @return 引用类型的装箱对象
+         */
+        template <typename U>
+        static auto MakeRef(BoxedObject<U> &other) noexcept
+            -> typename std::enable_if<_IsStaticCastable<U, T>::value, BoxedObject<T>>::type
+        {
+            BoxedObject<T> result{_IsRefParam{true}};
+            result._data.refptr = other.IsNullRef() ? nullptr : static_cast<T *>(&other.Unbox());
+            return result;
+        }
+
+        /**
+         * @brief 拷贝构造函数
+         * @param other 另一个装箱对象
+         */
+        BoxedObject(const BoxedObject &other) noexcept(std::is_nothrow_copy_constructible<T>::value)
+            : BoxedObject(_IsRefParam{other._isRef})
+        {
+            if (_isRef) {
+                _data.refptr = other._data.refptr;
+            } else {
+                new (_data.objbuf) T(other.Unbox());
+            }
+        }
+
+        /**
+         * @brief 移动构造函数
+         * @param other 另一个装箱对象
+         */
+        BoxedObject(BoxedObject &&other) noexcept(std::is_nothrow_move_constructible<T>::value)
+            : BoxedObject(_IsRefParam{other._isRef})
+        {
+            if (_isRef) {
+                _data.refptr       = other._data.refptr;
+                other._data.refptr = nullptr;
+            } else {
+                new (_data.objbuf) T(std::move(other.Unbox()));
+            }
+        }
+
+        /**
+         * @brief 拷贝赋值运算符
+         * @param other 另一个装箱对象
+         * @return 当前装箱对象的引用
+         */
+        BoxedObject &operator=(const BoxedObject &other)
+        {
+            if (this == &other)
+                return *this;
+
+            if (_isRef) {
+                if (other._isRef) {
+                    _data.refptr = other._data.refptr;
+                } else {
+                    auto oldPtr = _data.refptr;
+                    try {
+                        new (_data.objbuf) T(other.Unbox());
+                        _isRef = false;
+                    } catch (...) {
+                        _data.refptr = oldPtr;
+                        throw;
+                    }
+                }
+            } else {
+                if (other._isRef) {
+                    Release();
+                    _data.refptr = other._data.refptr;
+                    _isRef       = true;
+                } else {
+                    Unbox() = other.Unbox();
+                }
+            }
+            return *this;
+        }
+
+        /**
+         * @brief 移动赋值运算符
+         * @param other 另一个装箱对象
+         * @return 当前装箱对象的引用
+         */
+        BoxedObject &operator=(BoxedObject &&other)
+        {
+            if (this == &other)
+                return *this;
+
+            if (_isRef) {
+                if (other._isRef) {
+                    _data.refptr       = other._data.refptr;
+                    other._data.refptr = nullptr;
+                } else {
+                    auto oldPtr = _data.refptr;
+                    try {
+                        new (_data.objbuf) T(std::move(other.Unbox()));
+                        _isRef = false;
+                    } catch (...) {
+                        _data.refptr = oldPtr;
+                        throw;
+                    }
+                }
+            } else {
+                if (other._isRef) {
+                    Release();
+                    _data.refptr       = other._data.refptr;
+                    _isRef             = true;
+                    other._data.refptr = nullptr;
+                } else {
+                    Unbox() = std::move(other.Unbox());
+                }
+            }
+            return *this;
+        }
+
+        /**
+         * @brief 判断当前装箱对象是否为引用类型
+         * @return 若当前对象为引用类型则返回true，否则返回false
+         */
+        bool IsRef() const noexcept
+        {
+            return _isRef;
+        }
+
+        /**
+         * @brief 判断当前装箱对象是否为空引用
+         * @return 若当前对象为引用类型且引用指针为空则返回true，否则返回false
+         */
+        bool IsNullRef() const noexcept
+        {
+            return _isRef && (_data.refptr == nullptr);
+        }
+
+        /**
+         * @brief 判断当前装箱对象是否包含有效值
+         * @return 若当前对象包含有效值则返回true，否则返回false
+         */
+        bool HasValue() const noexcept
+        {
+            return !IsNullRef();
+        }
+
+        /**
+         * @brief 获取装箱对象中的值
+         * @return 装箱对象中的值的引用
+         * @note 仅当装箱对象包含有效值时调用此方法，否则行为未定义
+         */
+        T &Unbox() noexcept
+        {
+            assert(HasValue());
+            return _isRef ? *_data.refptr : *reinterpret_cast<T *>(_data.objbuf);
+        }
+
+        /**
+         * @brief 获取装箱对象中的值
+         * @return 装箱对象中的值的常量引用
+         * @note 仅当装箱对象包含有效值时调用此方法，否则行为未定义
+         */
+        const T &Unbox() const noexcept
+        {
+            assert(HasValue());
+            return _isRef ? *_data.refptr : *reinterpret_cast<const T *>(_data.objbuf);
+        }
+    };
+}
