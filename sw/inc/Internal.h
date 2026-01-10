d063300550ec6fe116a0895d404183a42290c0d9
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2026-01-10 20:53:07 +0800

    调整`_OptimalParamType`
    Updated the _OptimalParamType template to use std::decay, ensuring that parameter type deduction works correctly for types with cv-qualifiers and references. This makes the template more robust and consistent for various input types.

--- sw/inc/Internal.h
+++ sw/inc/Internal.h
@@ -8,6 +8,7 @@ namespace sw
      * @brief 最佳参数类型，标量类型使用值传递，复杂类型使用常量引用传递
      */
     template <typename T>
-    using _OptimalParamType =
-        typename std::conditional<std::is_scalar<T>::value, T, const T &>::type;
+    using _OptimalParamType = typename std::conditional<
+        std::is_scalar<typename std::decay<T>::type>::value,
+        typename std::decay<T>::type, const typename std::decay<T>::type &>::type;
 }
